1983083df7e8b62283052abf8d99113727e7ce1b
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2023-04-29 10:14:55 +0100

    add cart icon and align it to the right

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,6 +3,7 @@ export default function Header(){
     return(
         <header className="header-container">
             <div className='logo'>E-commerce</div>
+            <i class="fa-solid fa-cart-shopping"></i>
             <i class="fa-solid fa-bars"></i>
         </header>
     )
