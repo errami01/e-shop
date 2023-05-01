5d6eab56aa5dbc631244d491e89cb9fe88f5711d
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2023-05-01 19:27:15 +0100

    Add slider menu and style it

--- src/components/BarsMenu.js
+++ src/components/BarsMenu.js
@@ -1,6 +1,16 @@
 import './BarsMenu.css'
-export default function BarsMenu(){
+import { useRef } from 'react'
+export default function BarsMenu({isOpen}){
+    const container = useRef()
+    console.log(container.current)
     return(
-        <div className="barsMenu-container"></div>
+        <div 
+        className={`barsMenu-container ${isOpen? 'openedMenu':'closedMenu'}`}
+        ref={container}>
+            <ul>
+                <li>Sign In</li>
+                <li>Sign Up</li>
+            </ul>
+        </div>
     )
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,19 @@
 import './Header.css'
 import BarsMenu from './BarsMenu'
+import { useState } from 'react'
 export default function Header(){
+    const [isMenuOpen, setIsMenuOpen] = useState(false)
+    function handleBarsClick(){
+        setIsMenuOpen(prev=> !prev)
+        
+    }
+    // console.log(isMenuOpen)
     return(
         <header className="header-container">
             <div className='logo'>E-commerce</div>
             <i className="fa-solid fa-cart-shopping"></i>
-            <i className="fa-solid fa-bars"></i>
-            <BarsMenu />
+            <i className="fa-solid fa-bars" onClick={handleBarsClick}></i>
+            <BarsMenu isOpen={isMenuOpen}/>
         </header>
     )
 }
