baf6a2e21e3c6d934fd2263ff9eb6c5a515bb39e
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2023-05-11 15:23:04 +0100

    Fill rating stars based on the rate

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,6 +1,26 @@
 import './Product.css'
 export default function Product(product){
     const {image, title, rating}= product
+    function setStars(rate){
+        const starIcons = []
+        let counter=1;
+        while(counter <= rate){
+            starIcons.push(<i className="fa-solid fa-star"></i>)
+            counter++
+        }
+        const remaining = counter-rate
+        if( remaining < 1){
+            if(remaining > 0.75)  starIcons.push(<i className="fa-regular fa-star"></i>)
+            else if(remaining > 0.25)  starIcons.push(<i className="fa-solid fa-star-half"></i>)
+            else starIcons.push(<i className="fa-solid fa-star"></i>)
+        }
+        while(starIcons.length <5){
+            starIcons.push(<i className="fa-regular fa-star"></i>)
+        }
+        return starIcons
+    }
+    
+
     // console.log(rating)
     return(
         <div className="product-container">
@@ -11,11 +31,12 @@ export default function Product(product){
                 <div className="product-name">{title}</div>    
                 <div className="product-rating">
                     <span className='count'>({rating.count})</span>
+                    {/* <i className="fa-regular fa-star"></i>
                     <i className="fa-regular fa-star"></i>
                     <i className="fa-regular fa-star"></i>
                     <i className="fa-regular fa-star"></i>
-                    <i className="fa-regular fa-star"></i>
-                    <i className="fa-solid fa-star"></i>
+                    <i className="fa-solid fa-star"></i> */}
+                    {setStars(rating.rate)}
                     <span className='rate'>{rating.rate}</span>
                 </div>
             </section>
