6f3ca283d2a5c9edfc2a632618ecea5924ef4e4e
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2023-05-15 11:21:04 +0100

    Add styles to ProductDetails

--- src/pages/ProductDetails.js
+++ src/pages/ProductDetails.js
@@ -10,15 +10,18 @@ export default function PrdoductDetails(){
     const stars = useRate(product.rating.rate)
     return(
         <div className="productDetails-container">
-            <div className="img-section-prdctDetails">
-                <img className="prdct-img-prdctDetails" src={product.image} />
-            </div>
-            <div className="details-productDetails">
-                <h5 className="title-productDetails">{product.title}</h5>
-                <span>{Number.isInteger(product.price)? product.price+'.00':product.price}$</span>
-                <p className="descr-productDetails">{product.description}</p>
-                <span className="rating-productDetails">({product.rating.count}) {stars} {product.rating.rate}</span>
-            </div>
+            {/* <div className="grid-productDetails"> */}
+                <div className="img-section-prdctDetails">
+                    <img className="prdct-img-prdctDetails" src={product.image} />
+                </div>
+                <div className="details-productDetails">
+                    <h5 className="title-productDetails">{product.title}</h5>
+                    <span className="price-productDetails">{Number.isInteger(product.price)? product.price+'.00':product.price}$</span>
+                    <div className="descr-productDetails">{product.description}</div>
+                    <span className="rating-productDetails">({product.rating.count}) {stars} {product.rating.rate}</span>
+                    <button className="addToCart-prdctDetails">Add to Cart</button>
+                </div>
+            {/* </div> */}
         </div>
     )
 }
