1e6a06bfa9af2cc49ccd23d583e10119e2e445ad
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2023-05-15 11:43:16 +0100

    add cartItems state and pass it to Outlet

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,10 +1,13 @@
 import { Outlet } from "react-router-dom"
 import Header from "./Header"
+import { useState } from "react"
+
 export default function Layout(){
+    const [cartItems, setCartItems] = useState([])
     return(
         <>
-            <Header />
-            <Outlet />
+            <Header cartLength={cartItems.length}/>
+            <Outlet context={{cartItems: [cartItems, setCartItems]}}/>
         </>
        
     )
