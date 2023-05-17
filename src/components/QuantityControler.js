59711f3d560f5563c0eb11d6757a7e4f8986fecf
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2023-05-17 13:04:29 +0100

    Set widths and heiths and background color of quantityControler

--- src/components/QuantityControler.js
+++ src/components/QuantityControler.js
@@ -3,7 +3,7 @@ export  default function QuantityControler(props){
     return(
         <div className={`quantityControler-container ${props.className}`}>
             <span className="minus-quantityControler">-</span>
-            <span className="minus-quantityControler">5</span>
+            <span className="result-quantityControler">5</span>
             <span className="plus-quantityControler">+</span>
         </div>
     )
