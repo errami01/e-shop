6c2d21666476f851c93a5b44342feb01dc99dc9a
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2023-05-17 11:24:05 +0100

    Add props to set quantityControler container className

--- src/components/QuantityControler.js
+++ src/components/QuantityControler.js
@@ -1,6 +1,6 @@
-export  default function QuantityControler(){
+export  default function QuantityControler(props){
     return(
-        <div className="quantityControler-container">
+        <div className={`quantityControler-container ${props.className}`}>
             <span className="minus-quantityControler">-</span>
             <span className="minus-quantityControler">5</span>
             <span className="minus-quantityControler">+</span>
