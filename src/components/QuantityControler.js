7d7fd044b285afb14109299c8baa1c0c28d86e2c
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2023-05-17 12:15:30 +0100

    import css file to QuantityControler

--- src/components/QuantityControler.js
+++ src/components/QuantityControler.js
@@ -1,9 +1,10 @@
+import './QuantityControler.css'
 export  default function QuantityControler(props){
     return(
         <div className={`quantityControler-container ${props.className}`}>
             <span className="minus-quantityControler">-</span>
             <span className="minus-quantityControler">5</span>
-            <span className="minus-quantityControler">+</span>
+            <span className="plus-quantityControler">+</span>
         </div>
     )
 }
