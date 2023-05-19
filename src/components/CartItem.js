d2f0985f22e7aac2d4a968dbc7b68e14d1682527
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2023-05-19 19:19:27 +0100

    make QuantityControler functional to update cart items quantity

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -8,7 +8,7 @@ export default function CartItem(item){
                 <h5 className='item-title-cartItem'>{item.title}</h5>
                 <p className='delevery-date-cartItem'>Delivery between Wednesday, May 17, 2023 and Friday, May 19, 2023</p> 
                 <span className='price-carItem'>{Number.isInteger(item.price)? item.price+'.00':item.price}$</span>
-                <QuantityControler className='quantity-cartItem' quantity={item.orderedQuantity}/>
+                <QuantityControler itemId={item.id} className='quantity-cartItem' quantity={item.orderedQuantity}/>
                 <span className='trash-cartItem'><i className="fa-solid fa-trash-can"></i></span>
 
         </div>
--- src/components/QuantityControler.js
+++ src/components/QuantityControler.js
@@ -1,15 +1,34 @@
 import './QuantityControler.css'
 import { useOutletContext } from 'react-router-dom'
-//This component is rendered in CartItem.js
+//This component is rendered in CartItem.js and productDetails
 export  default function QuantityControler(props){
     const {cart} = useOutletContext()
-    const [cartItem, setCartItems] = cart
-    
+    const [cartItems, setCartItems] = cart
+    const targetItem = cartItems.filter(item=> item.id === props.itemId)[0]
+    const targetItemIndex = cartItems.indexOf(targetItem)
+    function handleQuantity(event){
+        const eventTargetValue = event.target.innerHTML
+        if(eventTargetValue === '-' && targetItem.orderedQuantity > 1){
+            return setCartItems(prev=>{
+                const rslt = [...prev]
+                rslt[targetItemIndex] = {...targetItem, orderedQuantity: targetItem.orderedQuantity - 1}
+                return rslt
+            })
+        }
+        if(eventTargetValue === '+'){
+            
+            setCartItems(prev=>{
+                const rslt = [...prev]
+                rslt[targetItemIndex] = {...targetItem, orderedQuantity: targetItem.orderedQuantity + 1}
+                return rslt
+            })
+        }
+    }
     return(
         <div className={`quantityControler-container ${props.className}`}>
-            <span className="minus-quantityControler">-</span>
-            <span className="result-quantityControler">{props.quantity}</span>
-            <span className="plus-quantityControler">+</span>
+            <span className="minus-quantityControler" onClick={handleQuantity} value='-'>-</span>
+            <span className="result-quantityControler">{targetItem.orderedQuantity}</span>
+            <span className="plus-quantityControler" onClick={handleQuantity}>+</span>
         </div>
     )
 }
