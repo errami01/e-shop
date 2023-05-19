94136f21516ff96b3ea1c9fd3ac5f994d2b1d6e2
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2023-05-19 10:52:46 +0100

    save the cart in localStorage

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,9 +1,13 @@
 import { Outlet } from "react-router-dom"
 import Header from "./Header"
-import { useState } from "react"
+import { useState, useEffect } from "react"
 
 export default function Layout(){
-    const [cartItems, setCartItems] = useState([])
+    const [cartItems, setCartItems] = useState(JSON.parse(localStorage.getItem('cart'))||[])
+    
+    useEffect(()=>{
+        localStorage.setItem('cart', JSON.stringify(cartItems))
+    },[cartItems])
     // console.log(cartItems[0])
     return(
         <>
