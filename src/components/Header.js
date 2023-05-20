8a5078b398a97da4604402ecff5252247fc350d3
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2023-05-20 16:26:37 +0100

    Display the number of items in the cart on cart icon

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import './Header.css'
 import BarsMenu from './BarsMenu'
 import { useState } from 'react'
 import { Link } from 'react-router-dom'
-export default function Header(){
+export default function Header(props){
     const [isMenuOpen, setIsMenuOpen] = useState(false)
     function handleBarsClick(){
         setIsMenuOpen(prev=> !prev)
@@ -12,8 +12,11 @@ export default function Header(){
     return(
         <header className="header-container">
             <div className='logo'>E-commerce</div>
-            <Link className='cartIconLink-header' to='cart'> <i className="fa-solid fa-cart-shopping"></i></Link>
-            <i className="fa-solid fa-bars" onClick={handleBarsClick}></i>
+            <Link className='cartIconLink-header' to='cart'>
+                 <i className="fa-solid fa-cart-shopping"></i>
+                 <span className='items-counter-header'>{props.cartItemsNumber}</span>
+            </Link>
+                 <i className="fa-solid fa-bars" onClick={handleBarsClick}></i>
             <BarsMenu isOpen={isMenuOpen}/>
         </header>
     )
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,17 +1,22 @@
 import { Outlet } from "react-router-dom"
 import Header from "./Header"
-import { useState, useEffect } from "react"
+import { useState, useEffect, useRef } from "react"
 
 export default function Layout(){
     const [cartItems, setCartItems] = useState(JSON.parse(localStorage.getItem('cart'))||[])
-    
+    const cartItemsNumber = useRef()
+    cartItemsNumber.current= countCartItems(cartItems)
+    function countCartItems(cart){
+       return cart.reduce((acc, curr)=> acc + curr.orderedQuantity,0)
+    }
+    console.log(cartItemsNumber.current)
     useEffect(()=>{
         localStorage.setItem('cart', JSON.stringify(cartItems))
     },[cartItems])
     // console.log(cartItems[0])
     return(
         <>
-            <Header cartLength={cartItems.length}/>
+            <Header cartItemsNumber={cartItemsNumber.current}/>
             <Outlet context={{cart: [cartItems, setCartItems]}}/>
         </>
        
