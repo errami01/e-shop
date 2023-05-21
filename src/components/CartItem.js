a5b8e83bb7c15e6a78c12372c73d52a5aecf8318
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2023-05-21 10:03:18 +0100

    Set image link to item details

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,5 +1,6 @@
 import './CartItem.css'
 import QuantityControler from './QuantityControler'
+import { Link } from 'react-router-dom'
 //This component is rendered in Cart.js
 export default function CartItem(props){
     const {item} = props
@@ -11,7 +12,9 @@ export default function CartItem(props){
     }
     return (
         <div className="cartItem-container">
-                <img className="img-cartItem" src={item.image}/>
+                <Link to={`/${item.category}/${item.id}`}>
+                    <img className="img-cartItem" src={item.image}/>
+                </Link>
                 <h5 className='item-title-cartItem'>{item.title}</h5>
                 <p className='delevery-date-cartItem'>Delivery between Wednesday, May 17, 2023 and Friday, May 19, 2023</p> 
                 <span className='price-carItem'>{Number.isInteger(item.price)? item.price+'.00':item.price}$</span>
