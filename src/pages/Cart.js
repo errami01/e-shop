4342a4cab01c0d16475971f97de126f42f2c8699
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2023-05-22 08:59:12 +0100

    Add total elements

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -8,12 +8,18 @@ export default function Cart(){
     const cartItemsElements = cartItems.map((item)=>{
         return <CartItem  key={nanoid()} item={item} cart={cart}/>
     })
+    const totalAmount = cartItems.reduce((acc, curr)=>  acc+(curr.price*curr.orderedQuantity),0)
     return (
         <div className="cart-container">
             <div className="items-cart">
                 <h2 className="title-cart">My Cart</h2>
                 {cartItemsElements}
+                <div className="total-cart">
+                    <span className="total-label-cart">Total</span>
+                    <span className="amount-cart">{totalAmount}</span>
+                </div>
             </div>
+            
            
         </div>
     )
