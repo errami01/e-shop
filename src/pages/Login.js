b9ef3dd01c99dcc143a7c61031bec34e23f7a06a
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2023-05-29 20:36:15 +0100

    add action function that console log a string

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,6 +1,11 @@
 import './Login.css'
 import InputComponent from '../components/InputComponent'
 import { Form } from 'react-router-dom'
+
+export function action(){
+    console.log('this is Action in action')
+    return null
+}
 export default function Login(){
 
     return(
@@ -10,18 +15,18 @@ export default function Login(){
                 <i className="fa-regular fa-user" ></i>
             </span>
             <h3 className='sign-in-urge--login'>Sign in to your account</h3>
-            <Form className='form--login'>
-            <InputComponent 
-                label='Username' 
-                type='text'
-                icon = {<i className="fa-solid fa-user fa" ></i>}
-                />
-            <InputComponent 
-            label='Password' 
-            type='password'
-            icon={<i className="fa-solid fa-key fa"></i>}
-            />  
-            <button className='submit-btn--login'>Login</button>
+            <Form method='post' className='form--login'>
+                <InputComponent 
+                    label='Username' 
+                    type='text'
+                    icon = {<i className="fa-solid fa-user fa" ></i>}
+                    />
+                <InputComponent 
+                label='Password' 
+                type='password'
+                icon={<i className="fa-solid fa-key fa"></i>}
+                />  
+                <button className='submit-btn--login'>Login</button>
             </Form>
         </div>
     )
