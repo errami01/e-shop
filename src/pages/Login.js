19bd56d1e702afabfd75c8ef1756b12754693bad
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2023-05-29 20:18:28 +0100

    wrap inputs in Form and style it

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,6 +1,8 @@
 import './Login.css'
 import InputComponent from '../components/InputComponent'
+import { Form } from 'react-router-dom'
 export default function Login(){
+
     return(
         <div className="login-container">
             
@@ -8,6 +10,7 @@ export default function Login(){
                 <i className="fa-regular fa-user" ></i>
             </span>
             <h3 className='sign-in-urge--login'>Sign in to your account</h3>
+            <Form className='form--login'>
             <InputComponent 
                 label='Username' 
                 type='text'
@@ -19,6 +22,7 @@ export default function Login(){
             icon={<i className="fa-solid fa-key fa"></i>}
             />  
             <button className='submit-btn--login'>Login</button>
+            </Form>
         </div>
     )
 }
