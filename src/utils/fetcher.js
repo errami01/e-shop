d63d708439ae2ee0f8603fd850c02f581e6c274a
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2023-05-31 00:42:47 +0100

    Add loginUser to get token

--- src/utils/fetcher.js
+++ src/utils/fetcher.js
@@ -10,3 +10,24 @@ export async function getProductsByCategory(category){
     // const products = await data.json()
     return await data.json()
 }
+export async function loginUser(user, pass){
+    const response = await fetch('https://fakestoreapi.com/auth/login',{
+            method:'POST',
+            headers: {
+                "Content-Type": "application/json",
+              },
+            
+            body:JSON.stringify({
+                username: 'mor_2314',
+                password: '83r5^_'
+            })
+        })
+    if(!response.ok){
+        throw{
+            message: response.status
+        }
+    }
+    
+    return await response.json()
+            
+}
