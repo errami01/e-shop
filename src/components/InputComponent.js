97717a2db80244a5a48afa2be95e2b1b87e7f7ba
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2023-05-31 00:41:02 +0100

    Add name attribute to form input

--- src/components/InputComponent.js
+++ src/components/InputComponent.js
@@ -4,7 +4,7 @@ export default function InputComponent(props){
     return (
     <div className='input-container'>
         {props.icon}
-        <input  className='input--inputComponent' type={props.type} placeholder=' '/>
+        <input name={props.name}  className='input--inputComponent' type={props.type} placeholder=' '/>
         <label className='label--inputComponent'>{props.label}</label>
         <div className='bottom-line--inputComponent'></div>
     </div>)
