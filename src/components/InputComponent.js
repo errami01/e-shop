b4626bcea65d94296810bc6eaa374b3b0ba202cb
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2023-06-11 12:47:48 +0100

    Add required attribute as option to InputComponent

--- src/components/InputComponent.js
+++ src/components/InputComponent.js
@@ -4,7 +4,13 @@ export default function InputComponent(props){
     return (
     <div className='input-container'>
         {props.icon}
-        <input name={props.name}  className='input--inputComponent' type={props.type} placeholder=' '/>
+        <input 
+            name={props.name}  
+            className='input--inputComponent' 
+            type={props.type} 
+            placeholder=' '
+            required = {props.required}
+        />
         <label className='label--inputComponent'>{props.label}</label>
         <div className='bottom-line--inputComponent'></div>
     </div>)
