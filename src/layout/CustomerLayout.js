8225d49f060fd04c7088aec4982b638b789b862f
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2023-06-12 12:16:28 +0100

    Add Outlet to CustomerLayout

--- src/layout/CustomerLayout.js
+++ src/layout/CustomerLayout.js
@@ -1,7 +1,9 @@
+import { Outlet } from "react-router-dom"
 export default function CustomerLayout(){
     return(
         <div className="customer-layout-container">
-
+            <h1>This is Customer Layout</h1>
+            <Outlet />
         </div>
     )
 }
