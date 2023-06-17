cf2eeb52a24252c45375a40482f086758cd9ba7f
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2023-06-17 16:19:24 +0100

    Set placeholder and styles for user infos containers in MyAccount page

--- src/pages/MyAccount.js
+++ src/pages/MyAccount.js
@@ -1,6 +1,50 @@
 
+import "./MyAccount.css"
+
+// export function loader({request}){
+//     console.log(request)
+//     return null
+// }
 export default function MyAccount(){
     return(
-        <h1>This is MyAccount</h1>
+        <div className="myAccount-container">
+            <h1 className="title--myAccount">Your Account</h1>
+            <div className="grid--myAccount">
+                <div className="info--myAccount">
+                    <header className="section-header--myAccount">
+                        PERSONAL INFORMATIONS
+                    </header>
+                    <div className="details-section--myAccount">
+                        <ul>
+                            <li></li>
+                            <li></li>
+                        </ul>
+                    </div>
+                </div>
+                <div className="info--myAccount">
+                    <header className="section-header--myAccount">
+                        ADDRESSES
+                    </header>
+                    <div className="details-section--myAccount">
+                        <ul>
+                            <li></li>
+                            <li></li>
+                        </ul>
+                    </div>
+                </div>
+                <div className="info--myAccount">
+                    <header className="section-header--myAccount">
+                        COMMUNICATIONS PREFERENCES
+                    </header>
+                    <div className="details-section--myAccount">
+                        <ul>
+                            <li></li>
+                            <li></li>
+                        </ul>
+                    </div>
+                </div>
+                
+            </div>
+        </div>
     )
 }
