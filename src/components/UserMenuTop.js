0a1b9c5f9bbad81b5a39a9da6bec570273e40646
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2023-06-20 16:46:08 +0100

    Create UserMenuTop function component and import css file

---
+++ src/components/UserMenuTop.js
@@ -0,0 +1,11 @@
+import './UserMenuTop.css'
+export default function UserMenuTop(){
+    return(
+        <ul className="container--userMenuTop">
+            <li>test</li>
+            <li>test</li>
+            <li>test</li>
+            <li>test</li>
+        </ul>
+    )
+}
