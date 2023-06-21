654ce298e9236184c6ca481b93474c8825141582
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2023-06-22 00:53:08 +0100

    Add click handler to userMenuTop container to stop click propagation

--- src/components/UserMenuTop.js
+++ src/components/UserMenuTop.js
@@ -1,7 +1,10 @@
 import './UserMenuTop.css'
 export default function UserMenuTop(){
+    function handleUserMenuTopClick(e){
+        e.stopPropagation()
+    }
     return(
-        <ul className="container--userMenuTop">
+        <ul className="container--userMenuTop" onClick={handleUserMenuTopClick}>
             <li>Your account</li>
             <li>Your orders</li>
             <li>Mailbox</li>
