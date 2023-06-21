c4790ecf993427217f2f7724c0fc1175c9d300ce
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2023-06-21 12:44:01 +0100

    Fix: replace placeholders with actual data in user top menu

--- src/components/UserMenuTop.js
+++ src/components/UserMenuTop.js
@@ -2,10 +2,11 @@ import './UserMenuTop.css'
 export default function UserMenuTop(){
     return(
         <ul className="container--userMenuTop">
-            <li>test</li>
-            <li>test</li>
-            <li>test</li>
-            <li>test</li>
+            <li>Your account</li>
+            <li>Your orders</li>
+            <li>Mailbox</li>
+            <li>Your wishlist</li>
+            <li>Logout</li>
         </ul>
     )
 }
