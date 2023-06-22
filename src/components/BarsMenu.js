63fb6300e9064c4b62dc1e440225ecd87d012d22
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2023-06-22 10:54:29 +0100

    Fix:change BarsMenu container's id

--- src/components/BarsMenu.js
+++ src/components/BarsMenu.js
@@ -17,7 +17,7 @@ export default function BarsMenu({isOpen, onMouseLeave, onMouseEnter, categories
     }
     return(
         <div 
-        id='bars-icon-container'
+        id='barsIconContainer'
         className={`barsMenu-container ${isOpen? 'openedMenu':'closedMenu'}`}
         ref={container}
         onMouseLeave={onMouseLeave}
