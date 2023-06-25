5e1042109aa8458b496fe58e40bc7a43ec8be66e
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2023-06-25 11:09:47 +0100

    Set menu user menu type based on isBig prop and set the default value for isBig and is Open

--- src/components/UserMenuTop.js
+++ src/components/UserMenuTop.js
@@ -1,15 +1,21 @@
 import './UserMenuTop.css'
 import { Link } from 'react-router-dom'
 export default function UserMenuTop(props){
+    const {isBig=false, isOpen=true} = props
     function handleUserMenuTopClick(e){
         e.stopPropagation()
     }
     return(
-        <ul className={`container--userMenuTop ${props.isOpen? 'openedMenu':'closedMenu'}`} onClick={handleUserMenuTopClick}>
-            <Link to={'customer'}>Your account</Link>
-            <Link to={'customer/orders'}>Your orders</Link>
-            <Link to={'customer/mailbox'}>Mailbox</Link>
-            <Link to={'customer/wishlist'}>Your Wishlist</Link>
+        <ul 
+            className={
+                `${isBig? 'big-container--userMenuTop':'container--userMenuTop'} 
+                ${isOpen? 'openedMenu':'closedMenu'}`} 
+            onClick={handleUserMenuTopClick}
+            >
+            <Link to={'/customer'}>Your account</Link>
+            <Link to={'/customer/orders'}>Your orders</Link>
+            <Link to={'/customer/mailbox'}>Mailbox</Link>
+            <Link to={'/customer/wishlist'}>Your Wishlist</Link>
             <Link >Logout</Link>
         </ul>
     )
