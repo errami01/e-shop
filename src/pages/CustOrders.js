bb1ac7660507a8a2edbe4c82684523691f8739bb
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2023-06-25 20:26:11 +0100

    Add open and closed orders NavLinks in CustOrders

--- src/pages/CustOrders.js
+++ src/pages/CustOrders.js
@@ -1,8 +1,13 @@
+import { NavLink } from "react-router-dom";
+
 export default function CustOrders(){
     return(
         <div className="custMailbox-container customer-page-container">
             <h1 className="title--customerLayout">Your orders</h1>
-
+            <ul>
+                <NavLink to='.'>Open orders</NavLink>
+                <NavLink to='closed'>Closed orders</NavLink>
+            </ul>
         </div>
     )
 }
