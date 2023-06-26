f9f8a55ec9f862349a8ef69678f5980db5d1133a
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2023-06-26 13:00:37 +0100

    Add style prop to nav bar NavLinks

--- src/pages/CustOrders.js
+++ src/pages/CustOrders.js
@@ -1,12 +1,29 @@
 import { NavLink } from "react-router-dom";
+import './CustOrders.css'
 
 export default function CustOrders(){
     return(
-        <div className="custMailbox-container customer-page-container">
+        <div className="custOrders-container customer-page-container">
             <h1 className="title--customerLayout">Your orders</h1>
             <ul className="nav-bar--custOrders">
-                <NavLink to='.'>Open orders</NavLink>
-                <NavLink to='closed'>Closed orders</NavLink>
+                <NavLink 
+                    to='.'
+                    style={(isActive)=>{
+                        return {
+                            color: isActive? 'var(--first-Color)':'grey',
+                            borderBottom: isActive? '1px solid var(--first-Color)':''
+                        }
+                    }}
+                    >OPEN ORDERS</NavLink>
+                <NavLink 
+                    to='closed'
+                    style={({isActive})=>{
+                        return {
+                            color: isActive? 'var(--first-Color)':'',
+                            borderBottom: isActive? '1px solid var(--first-Color)':''
+                        }
+                    }}
+                >CLOSED ORDERS</NavLink>
             </ul>
         </div>
     )
