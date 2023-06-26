1c7349c8601f89966e2e3733450a02a0297d502d
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2023-06-26 10:02:11 +0100

    Set class name for nav bar container in CustOrders

--- src/pages/CustOrders.js
+++ src/pages/CustOrders.js
@@ -4,7 +4,7 @@ export default function CustOrders(){
     return(
         <div className="custMailbox-container customer-page-container">
             <h1 className="title--customerLayout">Your orders</h1>
-            <ul>
+            <ul className="nav-bar--custOrders">
                 <NavLink to='.'>Open orders</NavLink>
                 <NavLink to='closed'>Closed orders</NavLink>
             </ul>
