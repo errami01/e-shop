54e047401ed93465d0a62440eb0e75916a7142a5
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2023-06-26 16:14:21 +0100

    Create open and closed orders components and return a placeholder from each

---
+++ src/pages/customer/ClosedOrders.js
@@ -0,0 +1,5 @@
+export default function ClosedOrders(){
+    return(
+        <h1>Closed orders go here</h1>
+    )
+}
---
+++ src/pages/customer/OpenOrders.js
@@ -0,0 +1,5 @@
+export default function OpenOrders(){
+    return(
+        <h1>Open orders go here</h1>
+    )
+}
