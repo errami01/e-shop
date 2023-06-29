3b0c95379dbe7270aa8e5826a2b8b81f8fe2a781
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2023-06-30 00:17:18 +0100

    Add a button and wrap it in Link that send to home page

--- src/components/EmptyOrders.js
+++ src/components/EmptyOrders.js
@@ -1,11 +1,15 @@
 
 import './EmptyOrders.css'
+import { Link } from 'react-router-dom'
 export default function EmptyOrders(props){
     return(
             <div className="container--EmptyOrders">
                 <i className="fa-solid fa-cart-flatbed-suitcase"></i>
                 <h5>{props.notification}</h5>
                 <p>{props.explanation}</p>
+                <Link to='/'>
+                  <button>{props.btnText}</button>
+                </Link>
             </div>  
     )
 }
