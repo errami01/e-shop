6fccaca1a14c62d3cc71bfd4ce8e28113401652b
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2023-07-01 18:50:59 +0100

    Set the icon prop for EmptyCustPage in ClosedOrders

--- src/pages/customer/ClosedOrders.js
+++ src/pages/customer/ClosedOrders.js
@@ -3,6 +3,7 @@ export default function ClosedOrders(){
     return(
         <div className='container--closedOrders'>
             <EmptyCustPage
+                icon={<i className="fa-solid fa-cart-flatbed-suitcase"></i>}
                 notification='No history to display'
                 explanation='All your closed orders will be saved here.'
                 btnText='START SHOPPING'
