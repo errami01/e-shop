094124d50d94ed663bcd64a8bbf901e0d9fbe150
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2023-07-01 19:05:39 +0100

    Set EmptyCustPage icon prop to flatbed cart icon

--- src/pages/customer/OpenOrders.js
+++ src/pages/customer/OpenOrders.js
@@ -2,7 +2,8 @@ import EmptyCustPage from '../../components/EmptyCustPage'
 export default function OpenOrders(){
     return(
         <div className="container--open-orders">
-            <EmptyCustPage 
+            <EmptyCustPage
+                icon={<i className="fa-solid fa-cart-flatbed-suitcase"></i>}
                 notification='You have not placed any orders!'
                 explanation='All your orders will be saved here so you can check their status at any time.'
                 btnText='CONTINUE SHOPPING'
