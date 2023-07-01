6627850d685bd5cfdf473d05627e10bedafedef8
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2023-07-01 10:09:50 +0100

    Render EmptyOrder insede ClosedOrders containerand set its props

--- src/pages/customer/ClosedOrders.js
+++ src/pages/customer/ClosedOrders.js
@@ -1,6 +1,12 @@
+import EmptyOrders from '../../components/EmptyOrders'
 export default function ClosedOrders(){
     return(
         <div className='container--closedOrders'>
+            <EmptyOrders
+                notification='No history to display'
+                explanation='All your closed orders will be saved here.'
+                btnText='START SHOPPING'
+            />
         </div>
     )
 }
