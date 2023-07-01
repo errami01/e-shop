ea31ed3fce891e65155bb565462af8c32ef595e6
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2023-07-01 19:35:20 +0100

    Render EmptyCustPage in CustWishlist and set its props

--- src/pages/customer/CustWishlist.js
+++ src/pages/customer/CustWishlist.js
@@ -1,8 +1,14 @@
+import EmptyCustPage from "../../components/EmptyCustPage"
 export default function CustWishlist(){
     return(
         <div className="custWishlist-container customer-page-container">
             <h1 className="title--customerLayout">Your Wishlist</h1>
-
+            <EmptyCustPage
+                icon={<i class="fa-solid fa-heart"></i>}
+                notification='Your wishlist is empty!'
+                explanation='Found something you like? Tap the heart icon next to the item to add it to your wishlist! All of your saved items will appear here.'
+                btnText='CONTINUE SHOPPING'
+            />
         </div>
     )
 }
