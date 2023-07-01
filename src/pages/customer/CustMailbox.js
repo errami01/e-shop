be2f397bdaaec56fb115b492ce15309d4c802f1f
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2023-07-01 19:00:33 +0100

    Change title text and render EmptyCustPage in Mailbox

--- src/pages/customer/CustMailbox.js
+++ src/pages/customer/CustMailbox.js
@@ -1,9 +1,14 @@
 
+import EmptyCustPage from "../../components/EmptyCustPage"
 export default function CustMailbox(){
     return(
         <div className="custMailbox-container customer-page-container">
-            <h1 className="title--customerLayout">Messages</h1>
-
+            <h1 className="title--customerLayout">Inbox</h1>
+            <EmptyCustPage 
+                icon={<i className="fas fa-envelope-open"></i>}
+                notification='You have no message'
+                explanation='Any messages we send you will be posted here. Stay connected'
+            />
         </div>
     )
 }
