82c02efccd7fee1303bbdbd9030399e28dde5e4f
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2023-07-02 20:15:53 +0100

    Remove token from local storage when calling setUserState without param

--- src/utils/useUserState.js
+++ src/utils/useUserState.js
@@ -3,7 +3,10 @@ import { useState } from "react";
 export function useUserState(initialState){
     const [local, setLocal] = useState(initialState)
     function setUserState(data){
-        if(!data) localStorage.removeItem('user') 
+        if(!data) {
+            localStorage.removeItem('user')
+            localStorage.removeItem('token')
+        }
         else localStorage.setItem('user', JSON.stringify(data))
         setLocal(JSON.parse(localStorage.getItem('user')))
     }
