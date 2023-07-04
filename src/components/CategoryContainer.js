0bdf2e5a1adcaa903fcfec8fd1bb39584d5dfa0d
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2023-07-04 20:01:20 +0100

    Remove Link from import statement

--- src/components/CategoryContainer.js
+++ src/components/CategoryContainer.js
@@ -1,5 +1,5 @@
 import { useState } from "react"
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import ProductCard from "./ProductCard";
 import './CategoryContainer.css'
 import {nanoid} from 'nanoid'
