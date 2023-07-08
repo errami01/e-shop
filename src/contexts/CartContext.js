ca43b50d97448a029cc000d45c5a97ec276c6c0e
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2023-07-08 11:20:52 +0100

    Add cart code and set return CartContext.Provder

--- src/contexts/CartContext.js
+++ src/contexts/CartContext.js
@@ -1,5 +1,19 @@
-import { createContext } from "react";
+import { useState, useEffect, useRef, createContext } from "react";
 
 export const CartContext = createContext();
-export default function CartContextProvider(){
+export default function CartContextProvider({children}){
+    const [cartItems, setCartItems] = useState(JSON.parse(localStorage.getItem('cart'))||[])
+    const cartItemsNumber = useRef()
+    cartItemsNumber.current= countCartItems(cartItems)
+    function countCartItems(cart){
+       return cart.reduce((acc, curr)=> acc + curr.orderedQuantity,0)
+    }
+    useEffect(()=>{
+        localStorage.setItem('cart', JSON.stringify(cartItems))
+    },[cartItems])
+    return(
+        <CartContext.Provider value={{cartItems, setCartItems,cartItemsNumber:cartItemsNumber.current}}>
+            {children}
+        </CartContext.Provider>
+    )
 }
