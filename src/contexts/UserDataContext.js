07641419fa665f952d336d1ce65312df81e3a37a
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2023-07-08 20:15:50 +0100

    Add userData state

--- src/contexts/UserDataContext.js
+++ src/contexts/UserDataContext.js
@@ -1,7 +1,9 @@
 import { createContext } from "react";
+import { useUserState } from "../utils/useUserState"
 
 export const UserDataContext = createContext();
 export default function UserDataContextProvider({children}){
+    const [userData, setUserData] = useUserState(()=>JSON.parse(localStorage.getItem('user')))
     return(
         <UserDataContext.Provider>
             {children}
