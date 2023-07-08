14e45ac39809762f5330a69ff50442ed51c1d3ec
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2023-07-08 11:24:25 +0100

    Wrap Layout in CartContextProvider and remove cart state related code

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -1,29 +1,18 @@
 import { Outlet, useLoaderData } from "react-router-dom"
 import Header from "../components/Header"
-import { useState, useEffect, useRef } from "react"
 import { fetchData } from "../utils/fetcher"
 import { useUserState } from "../utils/useUserState"
+import CartContextProvider from "../contexts/CartContext"
 
 export async function loader(){
     return await fetchData('categories')
 }
-
 export default function Layout(){
-    const [cartItems, setCartItems] = useState(JSON.parse(localStorage.getItem('cart'))||[])
     const [userData, setUserData] = useUserState(()=>JSON.parse(localStorage.getItem('user')))
-    const cartItemsNumber = useRef()
     const categories = useLoaderData()
-    cartItemsNumber.current= countCartItems(cartItems)
-    function countCartItems(cart){
-       return cart.reduce((acc, curr)=> acc + curr.orderedQuantity,0)
-    }
-    useEffect(()=>{
-        localStorage.setItem('cart', JSON.stringify(cartItems))
-    },[cartItems])
     return(
-        <>
+        <CartContextProvider>
             <Header 
-                cartItemsNumber={cartItemsNumber.current} 
                 categories={categories}
                 userData={userData}
                 setUserData={setUserData}
@@ -31,14 +20,12 @@ export default function Layout(){
             <Outlet 
                 context={
                     {
-                        cart: [cartItems, setCartItems], 
-                        cartItemsNumber: cartItemsNumber.current,
                         userData: userData,
                         setUserData: setUserData
                     }
                     }
                     />
-        </>
+        </CartContextProvider>
        
     )
 }
