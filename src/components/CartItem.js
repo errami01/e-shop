02354710097d1c105b886cbbc1b37a7644353dbd
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2023-07-10 11:12:42 +0100

    Add a className to style CartItem when displayed in cart overview

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -3,23 +3,24 @@ import QuantityControler from './QuantityControler'
 import { Link } from 'react-router-dom'
 //This component is rendered in Cart.js
 export default function CartItem(props){
-    const {item} = props
+    const {item, isPage} = props
     const {setCartItems} = props.cart
+    const pageClassName = isPage? '-page':''
     function handleTrashClick(){
         setCartItems(prev=>{
             return prev.filter(product=> product.id !== item.id)
         })
     }
     return (
-        <div className="cartItem-container">
-                <Link to={`/${item.category}/${item.id}`} className='image-link-cartItem'>
-                    <img className="img-cartItem" src={item.image} alt='cart item'/>
+        <div className={`cartItem-container${pageClassName}`}>
+                <Link to={`/${item.category}/${item.id}`} className={`image-link-cartItem${pageClassName}`}>
+                    <img className={`img-cartItem${pageClassName}`} src={item.image} alt='cart item'/>
                 </Link>
-                <h5 className='item-title-cartItem'>{item.title}</h5>
-                <p className='delevery-date-cartItem'>Delivery between Wednesday, May 17, 2023 and Friday, May 19, 2023</p> 
-                <span className='price-carItem'>{Number.isInteger(item.price)? item.price+'.00':item.price}$</span>
-                <QuantityControler itemId={item.id} className='quantity-cartItem' quantity={item.orderedQuantity}/>
-                <span className='trash-cartItem' onClick={handleTrashClick}>
+                <h5 className={`item-title-cartItem${pageClassName}`}>{item.title}</h5>
+                <p className={`delevery-date-cartItem${pageClassName}`}>Delivery between Wednesday, May 17, 2023 and Friday, May 19, 2023</p> 
+                <span className={`price-carItem${pageClassName}`}>{Number.isInteger(item.price)? item.price+'.00':item.price}$</span>
+                <QuantityControler itemId={item.id} className={`quantity-cartItem${pageClassName}`} quantity={item.orderedQuantity}/>
+                <span className={`trash-cartItem${pageClassName}`} onClick={handleTrashClick}>
                     <i className="fa-solid fa-trash-can"></i>
                 </span>
 
