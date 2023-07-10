42b664a21b03d76a4a78c87bd715d75fe55f1be1
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2023-07-10 11:53:52 +0100

    Import and nest Cart inside container

--- src/components/CartMenu.js
+++ src/components/CartMenu.js
@@ -1,7 +1,10 @@
-export default function CartMenu(){
+import './CartMenu.css'
+import Cart from './Cart'
+export default function CartMenu(props){
+    const {isOpen} = props
     return(
-        <div className="container--cartMenu">
-            
+        <div  className={`container--cartMenu ${!isOpen && 'closed--cartMenu'}`}>
+            <Cart />
         </div>
     )
 }
