fbea0a65b687f7152281850de04eb930638cabe9
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2023-07-12 10:40:29 +0100

    Prevent cartMenu click from reaching #cartMenu

--- src/components/CartMenu.js
+++ src/components/CartMenu.js
@@ -3,7 +3,10 @@ import Cart from './Cart'
 export default function CartMenu(props){
     const {isOpen} = props
     return(
-        <div  className={`container--cartMenu ${!isOpen && 'closed--cartMenu'}`}>
+        <div  
+            className={`container--cartMenu ${!isOpen && 'closed--cartMenu'}`}
+            onClick = {(e)=>e.stopPropagation()}
+        >
             <Cart withoutDeliveryDate/>
         </div>
     )
