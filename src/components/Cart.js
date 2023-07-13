b798cb10eba7f3f1ac12c7a60c9dee595b0ca0de
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2023-07-13 09:15:55 +0100

    Divide cart container in two section and restyle totals section

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -17,17 +17,23 @@ export default function Cart(props){
     })
     const totalAmount = cartItems.reduce((acc, curr)=>  acc+(curr.price*curr.orderedQuantity),0)
     return (
-            <div className="container-cart">
+            <div className="container--cart">
                 <div className="items-container--cart">
-                    <h2 className="title-cart">My Cart</h2>
+                    <h2 className="title--cart">My Cart</h2>
                     {cartItemsElements}
                 </div>
                 {
                     cartItemsNumber ? 
-                    <div className="total-cart">
-                        <span className="total-label-cart">Total :</span>
-                        <span className="amount-cart">{totalAmount.toFixed(2)}$</span>
-                        <button className="place-order-cart">Place order</button>
+                    <div className="totals--cart">
+                        <div className="total--cart items-total--cart">
+                            <span className="total-label--cart">Total items :</span>
+                            <span className="amount--cart">{totalAmount.toFixed(2)}$</span>
+                        </div>
+                        <div className="total--cart prices-total--cart">
+                            <span className="total-label--cart">TOTAL :</span>
+                            <span className="amount--cart">{totalAmount.toFixed(2)}$</span>
+                        </div>
+                        <button className="place-order--cart">Place order</button>
                     </div>
                     :
                     <h1 className="empty--cart">Your cart is empty</h1>
