19b9bc2842f44c8a123e9b729689280cc04c0895
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2023-07-14 19:30:02 +0100

    Add checkout phases list

--- src/layout/CheckoutLayout.js
+++ src/layout/CheckoutLayout.js
@@ -4,6 +4,13 @@ import Cart from "../components/Cart"
 export default function CheckoutLayout(){
     return(
         <div className="container--checkoutLayout">
+            <ul className='phaseses--checkoutLayout'>
+                <li>Account</li>
+                <li>-- <i className="fa-solid fa-circle-check"></i> --</li>
+                <li>Shipping</li>
+                <li>-- <i className="fa-solid fa-circle-check"></i> --</li>
+                <li>Payment</li>
+            </ul>
             <Outlet />
             <Cart />
         </div>
