cf8c1d4e296d1f305e8b55a1a3dbbceea88699ec
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2023-07-14 20:18:50 +0100

    Fix phases' className

--- src/layout/CheckoutLayout.js
+++ src/layout/CheckoutLayout.js
@@ -5,7 +5,7 @@ export default function CheckoutLayout(){
     return(
         <div className="container--checkoutLayout">
             <div className='checkout-section--checkoutLayout'>
-                <ul className='phaseses--checkoutLayout'>
+                <ul className='phases--checkoutLayout'>
                     <li>Account</li>
                     <li>-- <i className="fa-solid fa-circle-check"></i> --</li>
                     <li>Shipping</li>
