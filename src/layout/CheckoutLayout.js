9c4e94b25505cdae20703e725c3542678f7bca56
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2023-07-14 19:56:49 +0100

    Wrap Outlet and phaseses in checkout section and leave Cart component as a second section of the checkoutLayout container

--- src/layout/CheckoutLayout.js
+++ src/layout/CheckoutLayout.js
@@ -4,14 +4,16 @@ import Cart from "../components/Cart"
 export default function CheckoutLayout(){
     return(
         <div className="container--checkoutLayout">
-            <ul className='phaseses--checkoutLayout'>
-                <li>Account</li>
-                <li>-- <i className="fa-solid fa-circle-check"></i> --</li>
-                <li>Shipping</li>
-                <li>-- <i className="fa-solid fa-circle-check"></i> --</li>
-                <li>Payment</li>
-            </ul>
-            <Outlet />
+            <div className='checkout-section--checkoutLayout'>
+                <ul className='phaseses--checkoutLayout'>
+                    <li>Account</li>
+                    <li>-- <i className="fa-solid fa-circle-check"></i> --</li>
+                    <li>Shipping</li>
+                    <li>-- <i className="fa-solid fa-circle-check"></i> --</li>
+                    <li>Payment</li>
+                </ul>
+                <Outlet />
+            </div>     
             <Cart />
         </div>
     )
