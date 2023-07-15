a7527f1fcbbea69d1037e15ebdf3176f6962b6f3
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2023-07-16 00:20:30 +0100

    Add Form with personal infos labels and empty inputs

--- src/pages/checkout/PersonalInfos.js
+++ src/pages/checkout/PersonalInfos.js
@@ -1,8 +1,29 @@
+import { Form } from "react-router-dom"
 import CheckoutFlow from "../../components/CheckoutFlow"
 export default function PersonalInfos(){
     return(
         <CheckoutFlow>
-            <h1>Personal infos Go here</h1>
+            <h5>Account Details</h5>
+            <Form>
+                <label>
+                    First Name
+                    <input />
+                </label>
+                <label>
+                    Last Name
+                    <input />
+                </label>
+                <label>
+                    Phone
+                    <input />
+                </label>
+                <label>
+                    Email
+                    <input />
+                </label>
+                <button>Next</button>
+            </Form>
+            
         </CheckoutFlow>
         
     )
