846c29c7184d2dc93f80518fa7ccf3218c1cf40d
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2023-07-16 19:24:03 +0100

    Add default value attribute to form inputs

--- src/pages/checkout/PersonalInfos.js
+++ src/pages/checkout/PersonalInfos.js
@@ -1,25 +1,29 @@
 import { Form } from "react-router-dom"
 import CheckoutFlow from "../../components/CheckoutFlow"
+import { useContext } from "react"
+import { UserDataContext } from "../../contexts/UserDataContext"
 export default function PersonalInfos(){
+    const {userData} = useContext(UserDataContext)
+    const {name, phone, email} = userData
     return(
         <CheckoutFlow>
             <h5>Account Details</h5>
             <Form>
                 <label>
                     First Name
-                    <input />
+                    <input type='text' defaultValue={name.firstname}/>
                 </label>
                 <label>
                     Last Name
-                    <input />
+                    <input type='text' defaultValue={name.lastname}/>
                 </label>
                 <label>
                     Phone
-                    <input />
+                    <input defaultValue={phone} type='text'/>
                 </label>
                 <label>
                     Email
-                    <input />
+                    <input defaultValue={email} type='email'/>
                 </label>
                 <button className="btn--app">Next</button>
             </Form>
