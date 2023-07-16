86b7481a8c3c0d4790d7c5923c30232ffbe23c40
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2023-07-16 23:49:54 +0100

    Get userData from UserDataContext, add Form with its inputs and add action function that redirect to payment page

--- src/pages/checkout/Shipping.js
+++ src/pages/checkout/Shipping.js
@@ -1,5 +1,36 @@
+import CheckoutFlow from "../../components/CheckoutFlow";
+import { useContext } from "react";
+import { Form, redirect } from "react-router-dom";
+import { UserDataContext } from "../../contexts/UserDataContext";
+
+export function action(){
+    return redirect('payment')
+}
 export default function Shipping(){
+    const {userData} = useContext(UserDataContext)
+    const {number, street, city,zipcode} = userData.address
     return(
-        <h1>Shipping goes here</h1>
+        <CheckoutFlow>
+            <h5>Shipping details</h5>
+            <Form method="post">
+                <label>
+                    Address
+                    <input type='text' defaultValue={number} required/>
+                </label>
+                <label>
+                    Street
+                    <input type='text' defaultValue={street} required/>
+                </label>
+                <label>
+                    City
+                    <input defaultValue={city} type='text' required/>
+                </label>
+                <label>
+                    Postcode
+                    <input defaultValue={zipcode} type='text' required/>
+                </label>
+                <button className="btn--app">Next</button>
+            </Form>
+        </CheckoutFlow>
     )
 }
