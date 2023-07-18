383558ec4d1e0a1f2c1ec5567621c1878e69d6b2
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2023-07-18 09:24:13 +0100

    Add card expiration date and cvc input fields

--- src/pages/checkout/Payment.js
+++ src/pages/checkout/Payment.js
@@ -1,5 +1,6 @@
 import { Form } from "react-router-dom";
 import CheckoutFlow from "../../components/CheckoutFlow";
+import './Payment.css'
 
 export default function Payment(){
     return(
@@ -14,6 +15,21 @@ export default function Payment(){
                     Card number
                     <input type="tel"/>
                 </label>
+                <div className="expiration-cvc--payment">
+                    <label >
+                        Expiration
+                        <div className="card-expiration--payment">
+                            <input className="date--payment" type="number" placeholder="mm" min='1' max='12'/> 
+                            <span className="slash--payment">/</span>
+                            <input className='date--payment' type="number" placeholder="yy"/>
+                        </div>
+                    </label> 
+                    <label className="cvc--payment">
+                        CVC
+                        <input type="text"/>
+                    </label>
+                </div>
+                <button className="btn--app">Complete order</button>
             </Form>
         </CheckoutFlow>
         
