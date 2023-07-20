ce77bd578a95135c80a3535bca5210ed1e548ec3
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2023-07-20 12:08:52 +0100

    Get userData and setUserData from UserDataContext and set user data in useEffect

--- src/layout/CheckoutLayout.js
+++ src/layout/CheckoutLayout.js
@@ -3,13 +3,17 @@ import { Outlet, useLoaderData } from "react-router-dom"
 import Cart from "../components/Cart"
 import {requireAuth} from  '../utils/requireAuth'
 import { fetchSingleUser } from '../utils/fetcher'
+import { useContext, useEffect } from 'react'
+import { UserDataContext } from '../contexts/UserDataContext'
 
 export async function loader({request}){
     requireAuth(request)
     return await fetchSingleUser(1)     
 }
 export default function CheckoutLayout(){
+    const {userData, setUserData} = useContext(UserDataContext)
     const userLoadedData = useLoaderData()
+    useEffect(()=> setUserData(userLoadedData),[userLoadedData])
     return(
         <div className="container--checkoutLayout">
             <div className='infos-section--checkoutLayout'>
