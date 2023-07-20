a6e3774fe9226ad3e1bd466811dea39502b44b24
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2023-07-20 12:02:08 +0100

    Get single user data returned from loader using useLoader hook inside CheckoutLayout component

--- src/layout/CheckoutLayout.js
+++ src/layout/CheckoutLayout.js
@@ -1,12 +1,15 @@
 import './CheckoutLayout.css'
-import { Outlet } from "react-router-dom"
+import { Outlet, useLoaderData } from "react-router-dom"
 import Cart from "../components/Cart"
 import {requireAuth} from  '../utils/requireAuth'
+import { fetchSingleUser } from '../utils/fetcher'
 
 export async function loader({request}){
     requireAuth(request)
+    return await fetchSingleUser(1)     
 }
 export default function CheckoutLayout(){
+    const userLoadedData = useLoaderData()
     return(
         <div className="container--checkoutLayout">
             <div className='infos-section--checkoutLayout'>
