10ed4d08efb6ab0629495c5a06d3995f6bbc5b30
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2023-07-20 09:40:35 +0100

    Add a search param to the login path whenever the user navigates to the login page from a protected route

--- src/utils/requireAuth.js
+++ src/utils/requireAuth.js
@@ -1,6 +1,11 @@
 import { redirect } from "react-router-dom"
 
-export function requireAuth(path){
+export function requireAuth(request){
+    let path
+    if(request){
+        const url = new URL(request.url)
+        path = url.pathname
+    }
     const param = path? `?redirectTo=${path}` :''
     if(!JSON.parse(localStorage.getItem('token'))) {
         throw redirect(`/login${param}`)
