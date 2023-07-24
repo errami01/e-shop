6bae0afa08c2dbd709517412801895a2d1be9a16
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2023-07-24 22:03:15 +0100

    Fix setOngoingOrder logic error

--- src/utils/useOngoingOrder.js
+++ src/utils/useOngoingOrder.js
@@ -11,13 +11,16 @@ export default function useOngoingOrder(initialState){
     }
     return [ongoingOrder, setOngoingOrderState]
 }
-export function ongoingOrder(){
-    const setOngoingOrder = (ongoingOrder)=>{
-        if(!ongoingOrder){
-            localStorage.removeItem('ongoingOrder')
-        }
-        else localStorage.setItem('ongoingOrder',JSON.stringify(ongoingOrder))
+export function setOngoingOrder(newOngoingOrder){
+    let ongoingOrder
+    if(!newOngoingOrder){
+        localStorage.removeItem('ongoingOrder')
+        ongoingOrder = null
+        return ongoingOrder
     }
-    const ongoingOrder = JSON.parse(localStorage.getItem('ongoingOrder'))
-    return [ongoingOrder, setOngoingOrder]
+    console.log(newOngoingOrder instanceof Function)
+    newOngoingOrder = newOngoingOrder instanceof Function ? newOngoingOrder(JSON.parse(localStorage.getItem('ongoingOrder'))) : newOngoingOrder
+    localStorage.setItem('ongoingOrder',JSON.stringify(newOngoingOrder))
+    ongoingOrder = JSON.parse(localStorage.getItem('ongoingOrder'))
+    return ongoingOrder
 }
