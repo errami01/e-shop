060ecb9514bfb2bbb6e94fcf7e7777ea2e8889eb
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2023-07-27 11:46:50 +0100

    Return a NavLink if <to> prop  exist or <li> if not

--- src/components/CheckoutPhase.js
+++ src/components/CheckoutPhase.js
@@ -1,4 +1,17 @@
+import { NavLink } from "react-router-dom"
 
 export default function CheckoutPhase(props){
-    
+    const {to, children, ...rest} = props
+    return(
+        to ? 
+                    <NavLink 
+                    to={to}
+                    {...rest}
+                    >
+                        {children}
+                    </NavLink> : 
+                    <li className="phase-li--checkoutLayout">
+                        {children}
+                    </li>
+    )
 }
