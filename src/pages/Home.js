2383b23b39cb140ffa40fd62d7ad85299fc66e08
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2023-07-28 12:39:03 +0100

    Remove fetching products from loader and remove products prop from CategoryContainer

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,34 +5,18 @@ import "./Home.css"
 
 export async function loader(){
     const categories =  await fetchData('categories')
-    const products =  await fetchData('products')
-    return {categories, products}     
+    return {categories}     
 }
-
 export default function Home(){
     const loadedData = useLoaderData()
     const categoryElements = loadedData.categories.map(
         category=>{
-            const fiveArr = []
-            
-            loadedData.products.forEach(product=>{
-                if(fiveArr.length < 5){
-                    
-                    if(product.category===category){                     
-                        fiveArr.push(product)
-                    }
-                }else return
-            })
             return <CategoryContainer 
                     category={category}
-                    products={fiveArr}
                     key={category}
                     />
         }
-        
     )
-    
-    
     return(
         <div className="home-container">
         {categoryElements}
