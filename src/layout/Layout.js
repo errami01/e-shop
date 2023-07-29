029e87f1743cd5654550a84006f30c954c643023
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2023-07-29 12:19:21 +0100

    Remove loader function, Header props and fetchData()

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -1,23 +1,16 @@
-import { Outlet, useLoaderData, useLocation, useNavigate } from "react-router-dom"
+import { Outlet, useLocation, useNavigate } from "react-router-dom"
 import Header from "../components/Header"
-import { fetchData } from "../utils/fetcher"
 import CartContextProvider from "../contexts/CartContext"
 import UserDataContextProvider from "../contexts/UserDataContext"
 import { myHistory } from "../utils/myHistory"
 
-export async function loader(){
-    return await fetchData('categories')
-}
 export default function Layout(){
     myHistory.navigate = useNavigate()
     myHistory.location = useLocation()
-    const categories = useLoaderData()
     return(
         <UserDataContextProvider>
             <CartContextProvider>
-                <Header 
-                    categories={categories}
-                    />
+                <Header />
                 <Outlet />
             </CartContextProvider>
         </UserDataContextProvider>
