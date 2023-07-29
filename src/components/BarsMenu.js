2162faf4f77e773ed13bf0b0469de582471e4a19
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2023-07-29 12:11:30 +0100

    Add Await and Suspense

--- src/components/BarsMenu.js
+++ src/components/BarsMenu.js
@@ -1,17 +1,22 @@
 import './BarsMenu.css'
-import { useRef } from 'react'
+import { useRef, Suspense } from 'react'
 import {nanoid} from 'nanoid'
-import { Link } from 'react-router-dom'
+import { Link, Await } from 'react-router-dom'
+import { fetchData } from '../utils/fetcher'
+import Spinner from './Spinner'
 
-export default function BarsMenu({isOpen,categories}){
-    const container = useRef()
-    const categoryItems = categories.map(category=>{
-        const upperCaseFirstLetter = category.charAt(0).toUpperCase() + category.slice(1)
-        return (
-            <Link key={nanoid()} to={'category/'+category}>
-                <li >{upperCaseFirstLetter}</li>
-            </Link>)
-    })
+export default function BarsMenu({isOpen}){
+    const container = useRef()  
+    const awaitChild = (categories)=>{
+        const categoryItems = categories.map(category=>{
+            const upperCaseFirstLetter = category.charAt(0).toUpperCase() + category.slice(1)
+            return (
+                <Link key={nanoid()} to={'category/'+category}>
+                    <li >{upperCaseFirstLetter}</li>
+                </Link>)
+        })
+        return categoryItems
+    }
     function handleLocalStorage(){
         localStorage.clear()
     }
@@ -21,7 +26,11 @@ export default function BarsMenu({isOpen,categories}){
         ref={container}
         >
             <ul>
-                {categoryItems}
+                <Suspense fallback={<Spinner />}>
+                    <Await resolve={fetchData('categories')}>
+                        {awaitChild}
+                    </Await>
+                </Suspense>
                 <li onClick={handleLocalStorage}>Clear local storage</li>
             </ul>
         </div>
