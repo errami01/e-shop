77fcb5628ab8bd80e304fef7c92ce46155f0d9dd
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2023-07-30 22:07:55 +0100

    Add a div element that contain an icon tag and a span for text

--- src/layout/AdminLayout.js
+++ src/layout/AdminLayout.js
@@ -3,6 +3,10 @@ export default function AdminLayout(){
     return(
         <div className="container--adminLayout">
             <header>
+                <div className='logo--adminLayout'>
+                    <i className="fa-solid fa-user-check"></i>
+                    <span>Admin</span>
+                </div>
             </header>
         </div>
     )
