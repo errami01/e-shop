fd8ccf97c900a70c3c38259ed901bb81e5d3f44c
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2023-07-30 22:05:55 +0100

    Add header element

--- src/layout/AdminLayout.js
+++ src/layout/AdminLayout.js
@@ -2,6 +2,8 @@ import './AdminLayout.css'
 export default function AdminLayout(){
     return(
         <div className="container--adminLayout">
+            <header>
+            </header>
         </div>
     )
 }
