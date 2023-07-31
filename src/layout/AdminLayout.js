0b907e7193be0e40f831469267bcfa3d32a0ee2f
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2023-07-31 18:30:10 +0100

    Import and add AdminBody

--- src/layout/AdminLayout.js
+++ src/layout/AdminLayout.js
@@ -1,10 +1,12 @@
 import AdminHeader from '../components/AdminHeader'
+import AdminBody from '../components/AdminBody'
 import './AdminLayout.css'
 
 export default function AdminLayout(){
     return(
         <div className="container--adminLayout">
             <AdminHeader />
+            <AdminBody />
         </div>
     )
 }
