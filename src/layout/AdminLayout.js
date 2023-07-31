6f8736ccf267c5539a2b9c6673ff4ba3d5f3adfd
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2023-07-31 12:22:45 +0100

    Add AdminHeader component in AdminLayout

--- src/layout/AdminLayout.js
+++ src/layout/AdminLayout.js
@@ -1,8 +1,10 @@
+import AdminHeader from '../components/AdminHeader'
 import './AdminLayout.css'
+
 export default function AdminLayout(){
     return(
         <div className="container--adminLayout">
-            
+            <AdminHeader />
         </div>
     )
 }
