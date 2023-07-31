76fe11b168480347be59aa1f2f55e3a0548bb0eb
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2023-07-31 13:15:03 +0100

    Remove admin icon and add bars icon

--- src/components/AdminHeader.js
+++ src/components/AdminHeader.js
@@ -1,17 +1,19 @@
+import './AdminHeader.css'
 export default function AdminHeader(){
     return(
-        <header>
-                <div className='logo--adminLayout'>
-                    <i className="fa-solid fa-user-check"></i>
-                    <span>Admin</span>
-                </div>
-                <div role="search">
-                    <input aria-label="search term" placeholder='Search...'/>
-                    <button aria-label="search">
-                    <i className="fas fa-search"></i>
-                    </button>
-                </div>
-
-            </header>
+        <header className="container-adminHeader">
+            <div className='bars-icon-container--adminHeader'>
+                <i className="fa-solid fa-bars" ></i>
+            </div>
+            <div className='logo--adminLayout'>
+                <span>Admin</span>
+            </div>
+            <div role="search" className='search-container--adminHeader'>
+                <input aria-label="search term" placeholder='Search...'/>
+                <button aria-label="search">
+                <i className="fas fa-search"></i>
+                </button>
+            </div>
+        </header>
     )
 }
