fa09580f7e30f5d29f19b9c1de29a07654537896
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2023-07-31 20:07:49 +0100

    Remove page-title h1 default margin

--- src/components/AdminBody.js
+++ src/components/AdminBody.js
@@ -2,7 +2,7 @@ import './AdminBody.css'
 export default function AdminBody(){
     return(
         <div className="container--adminBody">
-            <h1 className='page-title-adminBody'>Page Title</h1>
+            <h1 className='page-title--adminBody'>Page Title</h1>
         </div>
     )
 }
