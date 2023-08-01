2a59aaf8917c91c803712b90880c907c5a22a262
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2023-08-01 11:45:33 +0100

    Add Outlet as AdminBody child

--- src/layout/AdminLayout.js
+++ src/layout/AdminLayout.js
@@ -1,12 +1,15 @@
 import AdminHeader from '../components/AdminHeader'
 import AdminBody from '../components/AdminBody'
 import './AdminLayout.css'
+import { Outlet } from 'react-router-dom'
 
 export default function AdminLayout(){
     return(
         <div className="container--adminLayout">
             <AdminHeader />
-            <AdminBody />
+            <AdminBody >
+                <Outlet />
+            </AdminBody>
         </div>
     )
 }
