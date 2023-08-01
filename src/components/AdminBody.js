782d836c25e23f4e15fbc1f44b59d939fd6e5312
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2023-08-01 13:07:41 +0100

    Render children below page title

--- src/components/AdminBody.js
+++ src/components/AdminBody.js
@@ -1,8 +1,9 @@
 import './AdminBody.css'
-export default function AdminBody(){
+export default function AdminBody({children}){
     return(
         <div className="container--adminBody">
             <h1 className='page-title--adminBody'>Page Title</h1>
+            {children}
         </div>
     )
 }
