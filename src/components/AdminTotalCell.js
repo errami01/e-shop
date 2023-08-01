614f2a1d69880d1766531bd6465e60f2fcc331b1
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2023-08-01 16:27:09 +0100

    Add icon, total label and total value elements

--- src/components/AdminTotalCell.js
+++ src/components/AdminTotalCell.js
@@ -1,6 +1,11 @@
-export default function AdminTotalCell(){
+export default function AdminTotalCell({iconClass, totalLabel, totalValue }){
     return(
         <div className="container--adminTotal-cell">
+            <i className={`${iconClass} icon--adminTotalCell`}></i>
+            <div>
+                <h5 className="cell-title--adminTotalCell">{totalLabel}</h5>
+                <p className="total-value--adminTotalCell">{totalValue}</p>
+            </div>
 
         </div>
     )
