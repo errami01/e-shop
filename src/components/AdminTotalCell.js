705b3a5dcf9f7659a8fc2a3e787749db9a914b61
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2023-08-02 00:24:00 +0100

    Add className for totals container

--- src/components/AdminTotalCell.js
+++ src/components/AdminTotalCell.js
@@ -3,7 +3,7 @@ export default function AdminTotalCell({iconClass, totalLabel, totalValue }){
     return(
         <div className="container--adminTotal-cell">
             <i className={`${iconClass} icon--adminTotalCell`}></i>
-            <div>
+            <div className='totals--adminTotalCell'>
                 <h5 className="cell-title--adminTotalCell">{totalLabel}</h5>
                 <p className="total-value--adminTotalCell">{totalValue}</p>
             </div>
