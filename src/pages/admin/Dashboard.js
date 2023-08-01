5a39f60b2f8dd8108e7834dd28ca3161430751ff
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2023-08-01 16:52:27 +0100

    Add AdminDashCell

--- src/pages/admin/Dashboard.js
+++ src/pages/admin/Dashboard.js
@@ -1,7 +1,12 @@
+import AdminDashCell from "../../components/AdminDashCell";
+import AdminTotalCell from "../../components/AdminTotalCell";
+
 export default function Dashboard(){
     return (
         <div className="container--dashboard">
-
+            <AdminDashCell>
+                
+            </AdminDashCell>  
         </div>
     )
 }
