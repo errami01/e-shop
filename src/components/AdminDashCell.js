fb675fa4cf32dcd935a74feafcd457b6d6e1093b
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2023-08-03 10:47:07 +0100

    Add className to title element and remove inline style from it

--- src/components/AdminDashCell.js
+++ src/components/AdminDashCell.js
@@ -2,7 +2,7 @@ import './AdminDashCell.css'
 export default function AdminDashCell({children,className, title}){
     return(
         <div className={`container--adminDashCell ${className}`} >
-            {title && <h1 style={{margin:0}}>{title}</h1>}
+            {title && <h1 className='cell-title--adminDashCell'>{title}</h1>}
             {children}
         </div>
     )
