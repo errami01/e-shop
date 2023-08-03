8d7a395193447b32f0faa9764ee3c8f98a10f375
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2023-08-03 09:29:20 +0100

    Add an h1 element to render title if it's provided in props

--- src/components/AdminDashCell.js
+++ src/components/AdminDashCell.js
@@ -1,7 +1,8 @@
 import './AdminDashCell.css'
-export default function AdminDashCell({children,className}){
+export default function AdminDashCell({children,className, title}){
     return(
         <div className={`container--adminDashCell ${className}`} >
+            {title && <h1 style={{margin:0}}>{title}</h1>}
             {children}
         </div>
     )
