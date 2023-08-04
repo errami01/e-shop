3c138ae5733e1e8d46cb2258a1ab3d2a31d5e2ed
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2023-08-04 19:44:16 +0100

    Fix icon class names

--- src/components/AdminSideMenu.js
+++ src/components/AdminSideMenu.js
@@ -4,23 +4,23 @@ export default function AdminSideMenu(){
     return(
         <div className="container--adminSideMenu">
             <NavLink to=''>
-                <i class="fa-solid fa-gauge-high"></i>
+                <i className="fa-solid fa-gauge-high"></i>
                 <span>Dashboard</span>
             </NavLink>
             <NavLink to='dashboard'>
-                <i class="fa-solid fa-gauge-high"></i>
+                <i className="fa-solid fa-gauge-high"></i>
                 <span>Dashboard</span>
             </NavLink>
             <NavLink to='dashboard'>
-                <i class="fa-solid fa-gauge-high"></i>
+                <i className="fa-solid fa-gauge-high"></i>
                 <span>Dashboard</span>
             </NavLink>
             <NavLink to='dashboard'>
-                <i class="fa-solid fa-gauge-high"></i>
+                <i className="fa-solid fa-gauge-high"></i>
                 <span>Dashboard</span>
             </NavLink>
             <NavLink to='dashboard'>
-                <i class="fa-solid fa-gauge-high"></i>
+                <i className="fa-solid fa-gauge-high"></i>
                 <span>Dashboard</span>
             </NavLink>
         </div>
