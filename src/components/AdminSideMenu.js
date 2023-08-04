1d28466a7ff99fe351916b9592525bc6b5b1c8d1
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2023-08-04 19:21:07 +0100

    Add NavLink with placeholders for menu items

--- src/components/AdminSideMenu.js
+++ src/components/AdminSideMenu.js
@@ -1,8 +1,28 @@
+import { NavLink } from 'react-router-dom'
 import './AdminSideMenu.css'
 export default function AdminSideMenu(){
     return(
         <div className="container--adminSideMenu">
-
+            <NavLink to=''>
+                <i class="fa-solid fa-gauge-high"></i>
+                <span>Dashboard</span>
+            </NavLink>
+            <NavLink to='dashboard'>
+                <i class="fa-solid fa-gauge-high"></i>
+                <span>Dashboard</span>
+            </NavLink>
+            <NavLink to='dashboard'>
+                <i class="fa-solid fa-gauge-high"></i>
+                <span>Dashboard</span>
+            </NavLink>
+            <NavLink to='dashboard'>
+                <i class="fa-solid fa-gauge-high"></i>
+                <span>Dashboard</span>
+            </NavLink>
+            <NavLink to='dashboard'>
+                <i class="fa-solid fa-gauge-high"></i>
+                <span>Dashboard</span>
+            </NavLink>
         </div>
     )
 }
