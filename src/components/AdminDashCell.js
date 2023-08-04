01ee68d027d969ab7101f3a988f66c6e00275a43
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2023-08-04 11:52:19 +0100

    Add className prop and set chart title text

--- src/components/AdminDashCell.js
+++
@@ -1,9 +0,0 @@
-import './AdminDashCell.css'
-export default function AdminDashCell({children,className, title}){
-    return(
-        <div className={`container--adminDashCell ${className}`} >
-            {title && <h1 className='cell-title--adminDashCell'>{title}</h1>}
-            {children}
-        </div>
-    )
-}
--- src/components/BarChart.js
+++ src/components/BarChart.js
@@ -2,16 +2,26 @@ import Chart from "chart.js/auto";
 import { CategoryScale } from "chart.js";
 import { Bar } from "react-chartjs-2";
 Chart.register(CategoryScale);
-export default function BarChart({chartData}){
+export default function BarChart({chartData, className}){
     return(
-    <div className="container--barChart">
+    <div className={className} >
       <Bar
         data={chartData}
         options={{
+          layout: {
+            padding: 10
+        },
           plugins: {
             title: {
               display: true,
-              text: ""
+              text: "Sales chart",
+              align: 'start',
+              font: {
+                size: 16
+              },
+              padding:{
+                bottom: 20
+              }
             },
             legend: {
               display: false
