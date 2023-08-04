50f502cd5fe8867ce5e3a66b6bca670a95ffb8a1
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2023-08-04 11:20:46 +0100

    Render AdminSideMenu

--- src/components/AdminBody.js
+++ src/components/AdminBody.js
@@ -1,9 +1,14 @@
 import './AdminBody.css'
+import AdminSideMenu from './AdminSideMenu'
 export default function AdminBody({children}){
     return(
         <div className="container--adminBody">
-            <h1 className='page-title--adminBody'>Page Title</h1>
-            {children}
+            <AdminSideMenu />
+            <div className='content--adminBody'>
+                <h1 className='page-title--adminBody'>Page Title</h1>
+                {children}
+            </div>
+            
         </div>
     )
 }
