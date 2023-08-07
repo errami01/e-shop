13c2e65477d1dbe7e49f4943e3f9df51de814b1e
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2023-08-07 19:16:32 +0100

    Add InputComponent and set its type prop to email

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,10 +1,13 @@
 import { Form } from "react-router-dom";
+import InputComponent from "../components/InputComponent";
 
 export default function SignUp(){
     return(
         <div className="container--signUp">
             <From>
-                
+                <InputComponent 
+                    type='email'
+                />
             </From>
         </div>
     )
