6e909313044934cf88aa98976fed39343289277a
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2023-08-09 00:39:40 +0100

    Implement checkEmail function and add it as email InputComponent onBlur event

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -5,6 +5,14 @@ import { useRef } from "react";
 export default function SignUp(){
     const emailInputElement = useRef()
     const emailContainerElement = useRef()
+    function checkEmail(){
+        const pattern = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/
+        if(!emailInputElement.current.value.match(pattern)){
+            emailContainerElement.current.style.setProperty('--theme-color', 'red')
+            emailContainerElement.current.setAttribute('invalidMessage', 'Please enter a valid email')
+            return 
+        }
+    }
     return(
         <div className="login-container">
             
@@ -22,6 +30,7 @@ export default function SignUp(){
                     label='Email' 
                     type='email'
                     icon = {<i className="fas fa-envelope"></i>}
+                    onBlur = {checkEmail}
                     required
                     />
                 <InputComponent 
