70f6ec176568ca50d4dbf098882233187810c136
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2023-08-09 11:06:49 +0100

    Use optional chaining to access refs instead of destructuring

--- src/components/InputComponent.js
+++ src/components/InputComponent.js
@@ -2,17 +2,16 @@
 import { forwardRef } from 'react'
 import './InputComponent.css'
 export const InputComponent = forwardRef(({label,icon, ...rest}, ref)=>{
-    const {containerRef, inputRef, labelRef, bottomLineRef} = ref
     return (
-    <div ref={containerRef} className='input-container invalid--inputComponent'>
+    <div ref={ref?.containerRef} className='input-container invalid--inputComponent'>
         {icon}
         <input 
             {...rest}
-            ref={inputRef}
+            ref={ref?.inputRef}
             className='input--inputComponent' 
             placeholder=' '
         />
-        <label ref={labelRef} className='label--inputComponent'>{label}</label>
-        <div ref={bottomLineRef} className='bottom-line--inputComponent '></div>
+        <label className='label--inputComponent'>{label}</label>
+        <div className='bottom-line--inputComponent '></div>
     </div>)
     })
