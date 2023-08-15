e1458e3dd2882577508f5ada1a1ae0b69398bc13
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2023-08-15 12:43:42 +0100

    Remove fetchData function

--- src/utils/fetcher.js
+++ src/utils/fetcher.js
@@ -31,39 +31,6 @@ export async function sleep(ms) {
         }, ms)
     })
 }
-export async function fetchData(param){
-    // await sleep(3000)
-    if(!param){
-        throw{
-            message: 'I should set a parameter to fetchData()'
-        }
-    }
-    const localData = JSON.parse(localStorage.getItem(param))
-    if(!localData){
-        let path;
-        if(param==='products'){
-            path= ''
-        }
-        else if(param==='categories'){
-            path = param
-        }
-        else{
-            path = `category/${param}`
-        }
-        const promiseData = await fetch(`https://fakestoreapi.com/products/${path}`)
-        if(!promiseData.ok){
-            throw{
-                message: "Failed to fetch ",
-                statusText: promiseData.statusText,
-                statas: promiseData.status
-            }
-        }
-        const data = await promiseData.json()
-        localStorage.setItem(param, JSON.stringify(data))
-        return data
-    }
-    return localData
-}
 export async function fetchSingleUser(id){
     // await sleep(3000)
     const localData = JSON.parse(localStorage.getItem('user'))
