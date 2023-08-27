be351a87f9efb9aa06705ebc98fb2b5832fd3cde
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2023-08-27 18:32:37 +0100

    Add auth query to the fetch url an change the user object structure

--- src/utils/authentication.js
+++ src/utils/authentication.js
@@ -4,17 +4,18 @@ import { createUserWithEmailAndPassword } from "firebase/auth";
 export async function signUp(email, pass){
     try{
         const credentials = await createUserWithEmailAndPassword(auth, email, pass)
-        fetch('https://e-commerce-8a744-default-rtdb.europe-west1.firebasedatabase.app/users.json',{
-            method: 'POST',
+        const idToken = await credentials.user.getIdToken()
+        localStorage.setItem('token', JSON.stringify(idToken))
+        fetch(`https://e-commerce-8a744-default-rtdb.europe-west1.firebasedatabase.app/users/${credentials.user.uid}.json?auth=${idToken}`,{
+            method: 'PUT',
             headers: {
                 'Content-Type': 'application/json'
             },
             body: JSON.stringify({
-                id:credentials.user.uid,
                 email:credentials.user.email
             })
         })
-        localStorage.setItem('token', JSON.stringify(await credentials.user.getIdToken()))
+        
     }
     catch(e){
         return e.message
