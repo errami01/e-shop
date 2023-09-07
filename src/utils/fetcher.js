8cd253c02f6928b5916f406cec5df253feb82c64
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2023-09-07 09:28:39 +0100

    Create myFetch() and use it to fetch data

--- src/utils/fetcher.js
+++ src/utils/fetcher.js
@@ -12,15 +12,7 @@ export async function sleep(ms) {
 export async function getProductsByCategory(category){
     const localData = JSON.parse(sessionStorage.getItem(category))
     if(!localData){
-        const promiseData = await fetch(`https://e-commerce-8a744-default-rtdb.europe-west1.firebasedatabase.app/products.json?orderBy="category"&equalTo="${category}"`)
-        if(!promiseData.ok){
-            throw{
-                message: "Failed to fetch Data",
-                statusText: promiseData.statusText,
-                statas: promiseData.status
-            }
-        }
-        const data = await promiseData.json()
+        const data = await myFetch(`https://e-commerce-8a744-default-rtdb.europe-west1.firebasedatabase.app/products.json?orderBy="category"&equalTo="${category}"`)
         sessionStorage.setItem(category, JSON.stringify(data))
         return data
     }
@@ -29,15 +21,7 @@ export async function getProductsByCategory(category){
 export async function getCategories(){
     const localData = JSON.parse(sessionStorage.getItem('categories'))
     if(!localData){
-        const promiseData = await fetch(`https://e-commerce-8a744-default-rtdb.europe-west1.firebasedatabase.app/categories.json`)
-        if(!promiseData.ok){
-            throw{
-                message: "Failed to fetch Data",
-                statusText: promiseData.statusText,
-                statas: promiseData.status
-            }
-        }
-        const data = await promiseData.json()
+        const data = await myFetch(`https://e-commerce-8a744-default-rtdb.europe-west1.firebasedatabase.app/categories.json`)
         sessionStorage.setItem('categories', JSON.stringify(data))
         return data
     }
@@ -47,15 +31,7 @@ export async function getSingleProduct(id){
     // await sleep(3000)
     const localData = JSON.parse(sessionStorage.getItem('products')) || {}
     if(!localData[id]){
-        const promiseData = await fetch(`https://e-commerce-8a744-default-rtdb.europe-west1.firebasedatabase.app/products.json?orderBy=%22id%22&equalTo=${id}`)
-        if(!promiseData.ok){
-            throw{
-                message: "Failed to fetch Data",
-                statusText: promiseData.statusText,
-                statas: promiseData.status
-            }
-        }
-        const data = await promiseData.json()
+        const data = await myFetch(`https://e-commerce-8a744-default-rtdb.europe-west1.firebasedatabase.app/products.json?orderBy=%22id%22&equalTo=${id}`)
         localData[id] = Object.values(data)[0]
         sessionStorage.setItem('products', JSON.stringify(localData))
         return localData[id]
@@ -67,17 +43,20 @@ export async function getUserData(id){
     const localData = JSON.parse(localStorage.getItem('user'))
     const idToken = getLocalIdToken()
     if(!localData){
-        const promiseData = await fetch(`https://e-commerce-8a744-default-rtdb.europe-west1.firebasedatabase.app/users/${id}.json?auth=${idToken}`)
-        if(!promiseData.ok){
-            throw{
-                message: "Failed to fetch Data",
-                statusText: promiseData.statusText,
-                statas: promiseData.status
-            }
-        }
-        const data = await promiseData.json()
+        const data = await myFetch(`https://e-commerce-8a744-default-rtdb.europe-west1.firebasedatabase.app/users/${id}.json?auth=${idToken}`)
         localStorage.setItem('user', JSON.stringify(data))
         return data
     }
     return localData
+}
+export async function myFetch(url){
+    const promiseData = await fetch(url)
+    if(!promiseData.ok){
+        throw{
+            message: "Failed to fetch Data",
+            statusText: promiseData.statusText,
+            statas: promiseData.status
+        }
+    }
+    return promiseData.json()
 }
