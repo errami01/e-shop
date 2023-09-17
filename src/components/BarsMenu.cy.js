0e1f8acb4d8def74f4db59a4366b088ab1966a4b
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2023-09-17 23:10:25 +0100

    Test that categories are rendered

--- src/components/BarsMenu.cy.js
+++ src/components/BarsMenu.cy.js
@@ -3,8 +3,13 @@ import BarsMenu from './BarsMenu'
 import { BrowserRouter } from 'react-router-dom'
 
 describe('<BarsMenu />', () => {
+  const data = ["tajin","kouskous","l7out","loubia"]
   beforeEach(()=>{
-    cy.intercept('GET', '/categories.json', ["tajin","kouskous","l7out","loubia"])
+    cy.mount(
+      <BrowserRouter>
+        <BarsMenu isOpen={true}/>
+      </BrowserRouter>)
+    cy.intercept('GET', '/categories.json', data)
   })
   
   it('should be hidden', ()=>{
@@ -15,11 +20,13 @@ describe('<BarsMenu />', () => {
     cy.getByData("barsMenuContainer").should('be.hidden')
   })
   it('should be visible', () => {
-    cy.mount(
-      <BrowserRouter>
-        <BarsMenu isOpen={true}/>
-      </BrowserRouter>)
     cy.getByData("barsMenuContainer").should('be.visible')
+  })
+  it('should render the list of categories',()=>{
+    cy.getByData("barsMenuContainer")
+      .should('be.visible')
+      .find('a')
+      .should('have.length', data.length)
   })
 
 })
