14b0a0c260908260b9784ebc4281073b8a0e9960
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2023-09-20 12:54:58 +0100

    Add id to go-to-cart button

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -35,7 +35,7 @@ export default function Cart(props){
                             <span className="amount--cart">{totalAmount.toFixed(2)}$</span>
                         </div>
                         <Link to={'/cart'}>
-                            <Button style={{width: '100%', marginTop: '10px'}}>Go to cart</Button>
+                            <Button id='go-to-cart-btn' style={{width: '100%', marginTop: '10px'}}>Go to cart</Button>
                         </Link>
                     </div>
                     :
