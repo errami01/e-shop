33893d36c7c264d86939b6660d207667869c2fad
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2023-09-22 16:22:52 +0100

    Comment useOngoingOrder funct

--- src/utils/useOngoingOrder.js
+++ src/utils/useOngoingOrder.js
@@ -1,16 +1,16 @@
 import { useState } from "react";
 
-export default function useOngoingOrder(initialState){
-    const [ongoingOrder, setOngoingOrder] = useState(initialState)
-    const setOngoingOrderState=(ongoingOrder)=>{
-        if(!ongoingOrder){
-            localStorage.removeItem('ongoingOrder')
-        }
-        else localStorage.setItem('ongoingOrder',JSON.stringify(ongoingOrder))
-        setOngoingOrder(JSON.parse(localStorage.getItem('ongoingOrder')))
-    }
-    return [ongoingOrder, setOngoingOrderState]
-}
+// export default function useOngoingOrder(initialState){
+//     const [ongoingOrder, setOngoingOrder] = useState(initialState)
+//     const setOngoingOrderState=(ongoingOrder)=>{
+//         if(!ongoingOrder){
+//             localStorage.removeItem('ongoingOrder')
+//         }
+//         else localStorage.setItem('ongoingOrder',JSON.stringify(ongoingOrder))
+//         setOngoingOrder(JSON.parse(localStorage.getItem('ongoingOrder')))
+//     }
+//     return [ongoingOrder, setOngoingOrderState]
+// }
 export function setOngoingOrder(newOngoingOrder){
     const initialValue = localStorage.getItem('ongoingOrder')
     let ongoingOrder
