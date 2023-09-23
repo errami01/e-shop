49c32149bc4a42b866fa21c29559d12bb9380b27
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2023-09-23 09:35:33 +0100

    Remove try catch blocks

--- src/utils/sendFormData.js
+++ src/utils/sendFormData.js
@@ -7,17 +7,11 @@ export default async function sendFormData(request){
     for (const key of formData.keys()) {
         toSend[key] = formData.get(key)
       }
-      
-    try{
-        await fetch(`https://e-commerce-8a744-default-rtdb.europe-west1.firebasedatabase.app/users/${user.uid}.json?auth=${idToken}`,{
-            method: 'PUT',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(toSend)
-        })
-    }
-    catch(e){
-        return e.message
-    }
+    await fetch(`https://e-commerce-8a744-default-rtdb.europe-west1.firebasedatabase.app/users/${user.uid}.json?auth=${idToken}`,{
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(toSend)
+    })
 }
