53da018fa676f719cf76ad3d9c96726899481ff0
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2023-09-23 23:21:01 +0100

    Add path argument to set path tho database

--- src/utils/sendFormData.js
+++ src/utils/sendFormData.js
@@ -1,5 +1,5 @@
 import { auth } from "../config/firbase";
-export default async function sendFormData(request){
+export default async function sendFormData(path, request){
     const user =auth.currentUser
     const idToken = await user.getIdToken()
     const formData = await request.formData()
@@ -7,7 +7,7 @@ export default async function sendFormData(request){
     for (const key of formData.keys()) {
         toSend[key] = formData.get(key)
       }
-    await fetch(`https://e-commerce-8a744-default-rtdb.europe-west1.firebasedatabase.app/users/${user.uid}.json?auth=${idToken}`,{
+    await fetch(`https://e-commerce-8a744-default-rtdb.europe-west1.firebasedatabase.app/${path}/${user.uid}.json?auth=${idToken}`,{
         method: 'PUT',
         headers: {
             'Content-Type': 'application/json'
