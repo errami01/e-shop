75fe8d0b6ca93377df617a151c8d278b745dfe99
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2023-09-25 18:24:56 +0100

    Import getuserData func from fetcher

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -3,6 +3,7 @@ import Header from "../components/Header"
 import CartContextProvider from "../contexts/CartContext"
 import UserDataContextProvider from "../contexts/UserDataContext"
 import { myHistory } from "../utils/myHistory"
+import { getUserData } from "../utils/fetcher"
 
 export async function loader(){
 }
