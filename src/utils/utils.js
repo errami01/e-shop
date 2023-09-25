2f51d33b3db00736aae744210ccff69d202411c8
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2023-09-25 21:31:29 +0100

    Throw an exception if the PUT request fails in storeObject function

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -27,13 +27,19 @@ export async function storeObject(data, path ,storeInLocalStorage){
     if(path){
         const user =auth.currentUser
         const idToken = await user.getIdToken()
-        await fetch(`https://e-commerce-8a744-default-rtdb.europe-west1.firebasedatabase.app/${path}/${user.uid}.json?auth=${idToken}`,{
+        const response = await fetch(`https://e-commerce-8a744-default-rtdb.europe-west1.firebasedatabase.app/${path}/${user.uid}.json?auth=${idToken}`,{
         method: 'PUT',
         headers: {
             'Content-Type': 'application/json'
         },
         body: JSON.stringify(data)
         })
+        if(!response.ok){
+            throw{
+                message : response.statusText,
+                status: response.status
+            }
+        }
     }
     
 }
