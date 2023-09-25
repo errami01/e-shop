2834a8b60cff0523c88de0a495e3c2505069bbdb
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2023-09-25 18:47:56 +0100

    Use useLoaderData hook to get userData returned from loader

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -1,4 +1,4 @@
-import { Outlet, useLocation, useNavigate } from "react-router-dom"
+import { Outlet, useLoaderData, useLocation, useNavigate } from "react-router-dom"
 import Header from "../components/Header"
 import CartContextProvider from "../contexts/CartContext"
 import UserDataContextProvider from "../contexts/UserDataContext"
@@ -12,6 +12,7 @@ export async function loader(){
 export default function Layout(){
     myHistory.navigate = useNavigate()
     myHistory.location = useLocation()
+    const userData = useLoaderData()
     return(
         <UserDataContextProvider>
             <CartContextProvider>
