ce6df24542e1dd96b4e1e1e8d23fe3b37dfb5402
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2023-09-28 08:00:36 +0100

    Remove suspense, Await and userData

--- src/layout/CustomerLayout.js
+++ src/layout/CustomerLayout.js
@@ -1,37 +1,19 @@
-import { Await, Outlet, defer, useLoaderData} from "react-router-dom"
+import { Outlet } from "react-router-dom"
 import "./CustomerLayout.css"
-import { getUserData } from "../utils/fetcher"
-import { Suspense } from "react"
+
 import { requireAuth } from "../utils/requireAuth"
 import UserMenuTop from "../components/UserMenuTop"
-import { useContext } from "react"
-import { UserDataContext } from "../contexts/UserDataContext"
-import Spinner from "../components/Spinner"
-import { UpdateState } from "../components/UpdateState"
 
 export function loader(){
     requireAuth()
-    return defer({userDataPromise: getUserData('"drLAqnXla5dpjz1izbyxU4jOuXe2"')})      
+    return null   
 }
 export default function CustomerLayout(){
-    const {userData, setUserData} = useContext(UserDataContext)
-    const {userDataPromise} = useLoaderData()    
-    const awaitChild = (userLoadedData)=>{
-        return(
-            <>
-                <UpdateState setState={setUserData} data={userLoadedData}/>
-                {userData && <Outlet />}
-            </>
-        )
-    }
+    
     return(
         <div className="customer-layout-container">
-            <UserMenuTop isBig={true} setUserData={setUserData}/>
-            <Suspense fallback={<Spinner />}>
-                <Await resolve={userDataPromise}>
-                    {awaitChild}
-                </Await>
-            </Suspense>
+            <UserMenuTop isBig={true} />
+            <Outlet />
         </div>
     )
 }
