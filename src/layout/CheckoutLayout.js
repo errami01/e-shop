d3ca4bac30a71223db80c10adeb3260a47055e81
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2023-09-28 09:25:40 +0100

    Remove UpdateState component and UserDataContext hook

--- src/layout/CheckoutLayout.js
+++ src/layout/CheckoutLayout.js
@@ -3,20 +3,17 @@ import { Await, Outlet, defer, useLoaderData } from "react-router-dom"
 import Cart from "../components/Cart"
 import {requireAuth} from  '../utils/requireAuth'
 import { getUserData } from '../utils/fetcher'
-import { useContext, useEffect, Suspense } from 'react'
-import { UserDataContext } from '../contexts/UserDataContext'
+import { useContext, Suspense } from 'react'
 import { CartContext } from '../contexts/CartContext'
 import { setOngoingOrder } from '../utils/useOngoingOrder'
 import CheckoutPhase from '../components/CheckoutPhase'
 import Spinner from '../components/Spinner'
-import { UpdateState } from '../components/UpdateState'
 
 export async function loader({request}){
     requireAuth(request)
     return defer({userDataPromise: getUserData(`"drLAqnXla5dpjz1izbyxU4jOuXe2"`)})     
 }
 export default function CheckoutLayout(){
-    const {userData, setUserData} = useContext(UserDataContext)
     const cart = useContext(CartContext)
     const {userDataPromise} = useLoaderData()
     const [ongoingOrder, updateOngoingOrder] = setOngoingOrder({
@@ -35,10 +32,8 @@ export default function CheckoutLayout(){
     const awaitChild = (userLoadedData)=>{
         return(
             <>
-                <UpdateState data={userLoadedData} setState={setUserData}/>
-                {userData && <Outlet context={{ ongoingOrder, cancelOrder }}/>}
-            </>
-           
+                <Outlet context={{ ongoingOrder, cancelOrder }}/>
+            </> 
         )
     }
     return(
