bedeca95c2950057508fe0b82cf784260091aa58
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2023-09-28 08:49:17 +0100

    Remove userData const and UserDataContextProvider

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -1,7 +1,6 @@
-import { Outlet, useLoaderData, useLocation, useNavigate } from "react-router-dom"
+import { Outlet, useLocation, useNavigate } from "react-router-dom"
 import Header from "../components/Header"
 import CartContextProvider from "../contexts/CartContext"
-import UserDataContextProvider from "../contexts/UserDataContext"
 import { myHistory } from "../utils/myHistory"
 import { getUserData } from "../utils/fetcher"
 import { confirmUserState, removeLocalUserData } from "../utils/utils"
@@ -21,13 +20,10 @@ export async function loader(){
 export default function Layout(){
     myHistory.navigate = useNavigate()
     myHistory.location = useLocation()
-    const userData = useLoaderData()
     return(
-        <UserDataContextProvider>
             <CartContextProvider>
-                <Header userData={userData}/>
+                <Header />
                 <Outlet />
             </CartContextProvider>
-        </UserDataContextProvider> 
     )
 }
