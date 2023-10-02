4a7c8118d88200707223e0ebf169d4a6164309ef
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2023-10-02 23:12:06 +0100

    Replace all data from cartContext by data from getLocalCart

--- src/components/QuantityControler.js
+++ src/components/QuantityControler.js
@@ -1,28 +1,38 @@
 import './QuantityControler.css'
 import { useContext } from 'react'
 import { CartContext } from '../contexts/CartContext'
+import { getLocalCart, setLocalCart, storeObject } from '../utils/utils'
+import { myHistory } from '../utils/myHistory'
 //This component is rendered in CartItem.js and productDetails
 export  default function QuantityControler(props){
-    const cart = useContext(CartContext)
-    const {cartItems, setCartItems} = cart
+    const cartItems= getLocalCart()
     const targetItem = cartItems.filter(item=> item.id === props.itemId)[0]
     const targetItemIndex = cartItems.indexOf(targetItem)
-    function handleQuantity(event){
+    async function handleQuantity(event){
         const eventTargetValue = event.target.innerHTML
         if(eventTargetValue === '-' && targetItem.orderedQuantity > 1){
-            return setCartItems(prev=>{
-                const rslt = [...prev]
-                rslt[targetItemIndex] = {...targetItem, orderedQuantity: targetItem.orderedQuantity - 1}
-                return rslt
-            })
+            const newCartItems = [...cartItems]
+            newCartItems[targetItemIndex] = {...targetItem, orderedQuantity: targetItem.orderedQuantity - 1}
+            try{
+                await storeObject(newCartItems, 'carts', setLocalCart)
+                myHistory.navigate('#', {replace: true})
+            }
+            catch(e){
+
+            }
+            return null 
         }
-        if(eventTargetValue === '+'){
+        if(eventTargetValue === '+'){ 
+            const newCartItems = [...cartItems]
+            newCartItems[targetItemIndex] = {...targetItem, orderedQuantity: targetItem.orderedQuantity + 1}
+            try{
+                await storeObject(newCartItems, 'carts', setLocalCart)
+                myHistory.navigate('#', {replace: true})
+            }
+            catch(e){
+
+            }
             
-            setCartItems(prev=>{
-                const rslt = [...prev]
-                rslt[targetItemIndex] = {...targetItem, orderedQuantity: targetItem.orderedQuantity + 1}
-                return rslt
-            })
         }
     }
     return(
