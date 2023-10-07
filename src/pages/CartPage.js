d41703b700ebb20bd0f3370d6663628092b6992b
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2023-10-07 11:58:34 +0100

    Extract cart from Outlet context and pass it to Cart component

--- src/pages/CartPage.js
+++ src/pages/CartPage.js
@@ -1,9 +1,11 @@
 import './CartPage.css'
 import Cart from '../components/Cart'
+import { useOutletContext } from 'react-router-dom'
 export default function CartPage(){
+  const {cart} = useOutletContext()
     return (
         <div className="container--cartPage">
-          <Cart isPage />
+          <Cart isPage cart={cart}/>
         </div>
     )
 }
