35adb8dda0cd8f112a0d60a1f357f6ef846c159e
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2023-10-12 09:24:54 +0100

    Wrap inputField in Form

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,7 +1,11 @@
+import { Form } from "react-router-dom";
+
 export default function SearchBar({inputField, ...rest}){
     return(
         <div {...rest}>
+            <Form>
                 {inputField}
+            </Form>
         </div>
     )
 }
