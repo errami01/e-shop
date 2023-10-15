b726d2221b13623ec9ec57f0c2006ae79ad9a398
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2023-10-15 23:21:41 +0100

    Unpack className from props and pass it to spinner container

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -1,7 +1,7 @@
 import './Spinner.css'
-export default function Spinner(){
+export default function Spinner({className}){
     return(
-        <div className="spinner--container">
+        <div className={`spinner--container ${className}`}>
             <div className="spinner"></div>
         </div>
     )
