d43fc55e7b986b00e44d87aab11c29c53dd0a4a2
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2023-10-20 17:43:08 +0100

    Create action function that is called to remove items from cart

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -1,8 +1,8 @@
-import { Outlet, useLoaderData, useLocation, useNavigate} from "react-router-dom"
+import { Outlet, redirect, useLoaderData, useLocation, useNavigate} from "react-router-dom"
 import Header from "../components/Header"
 import { myHistory } from "../utils/myHistory"
 import { getCart, getUserData } from "../utils/fetcher"
-import { removeLocalUserData } from "../utils/utils"
+import { removeLocalUserData, setLocalCart, storeObject } from "../utils/utils"
 import { confirmUserState } from "../utils/authentication"
 
 export async function loader(){
@@ -14,12 +14,29 @@ export async function loader(){
         //Remvove user data if the user session expires
         removeLocalUserData()
     }
+    console.log('Layout loader')
     return await getCart()
+}
+export async function action({request}){
+    console.log('Layout action')
+    const url = new URL(request.url)
+    const pathToGo = url.searchParams.get('redirect')
+    try{
+        const formData = await request.formData()
+        const newCartItems = JSON.parse(formData.get('newCart'))
+        await storeObject(newCartItems, 'carts', setLocalCart)
+        return redirect(pathToGo)
+    }
+    catch(e){
+        return e.message
+    }
+    return null
 }
 export default function Layout(){
     myHistory.navigate = useNavigate()
     myHistory.location = useLocation()
     const cart = useLoaderData()
+    console.log('layout component')
     return(
         <>
             <Header cart={cart}/>
