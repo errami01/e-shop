84052716bf788734c1624ee3d07916082277503c
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2023-10-25 10:18:40 +0100

    Render three span elements if the children prop is null

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,7 +2,14 @@ import './Button.css'
 export default function Button({children,className, ...rest}){
     return (
         <button className={`btn--Button ${className}`} {...rest}>
-            {children}
+            {children 
+            || 
+            <>
+                <span></span>
+                <span></span>
+                <span></span>
+            </>
+            }
         </button>
     )
 }
