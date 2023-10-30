846d47f74e16392d75c1d0cb1a2abc7246433b6b
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2023-10-30 23:58:09 +0100

    Remove Suspense and Await components and fetch data in useEffect

--- src/components/CategoryContainer.js
+++ src/components/CategoryContainer.js
@@ -1,5 +1,5 @@
-import { useState, Suspense } from "react"
-import { useNavigate, Await } from "react-router-dom";
+import { useState, useEffect } from "react"
+import { useNavigate } from "react-router-dom";
 import ProductCard from "./ProductCard";
 import './CategoryContainer.css'
 import {nanoid} from 'nanoid'
@@ -7,6 +7,7 @@ import { getProductsByCategory } from "../utils/fetcher";
 import ProductCardSkeleton from "./ProductCardSkeleton";
 
 export default function CategoryContainer({category}){
+    const [products, setProducts] = useState()
     const nbreOfItemToRender = 4
     const navigate = useNavigate()
     
@@ -17,6 +18,13 @@ export default function CategoryContainer({category}){
         }
         return skeletons
     }
+    useEffect(()=>{
+        if(!products) {
+            getProductsByCategory(category)
+            .then(value=> setProducts(value))
+        }
+    },[])
+    const itemCards = (products)=>{
         products = Object.values(products)
         products.length = nbreOfItemToRender
         const cards = products.map(
@@ -24,17 +32,17 @@ export default function CategoryContainer({category}){
         )
         return cards
     }
-    return(<>
+    return(
         <div className="category-container">
             <header className="category-name" onClick={()=> navigate(`category/${category}`)}>{category.toUpperCase()}</header>
             <div className="products-container">
-                <Suspense fallback={<Spinner />}>
-                    <Await resolve={getProductsByCategory(category)}>
-                        {awaitChild}
-                    </Await>
-                </Suspense>
+                {
+                    products?
+                        itemCards(products)
+                    :
+                        cardSkeletons(5)
+                }
             </div>
         </div>
-        </>
     )
 }
