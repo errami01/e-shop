b5bf3db54dda0e5b44d3cbf4d80a89fcd50c55c5
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2023-11-01 10:12:00 +0100

    Set the delevery date using getDeliveryDate func

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -3,9 +3,11 @@ import QuantityControler from './QuantityControler'
 import { Link, useNavigation } from 'react-router-dom'
 import { useEffect } from 'react'
 import Spinner from './Spinner'
+import { getDeliveryDate } from '../utils/utils'
 //This component is rendered in Cart.js
 export default function CartItem(props){
     const navigation = useNavigation()
+    const {day, month, year} = getDeliveryDate(3)
     const {
         item,
         isPage,
@@ -28,9 +30,9 @@ export default function CartItem(props){
                     <img className={`img-cartItem${pageClassName}`} src={item.image} alt='cart item'/>
                 </Link>
                 <h5 className={`item-title-cartItem${pageClassName}`}>{item.title}</h5>
-                {/* {!withoutDeliveryDate && <p className={`delevery-date-cartItem${pageClassName}`}>
-                    Delivery between Wednesday, May 17, 2023 and Friday, May 19, 2023
-                </p>}  */}
+                {!withoutDeliveryDate && <p className={`delevery-date-cartItem${pageClassName}`}>
+                    Delivery on {day.deleveryWeekday}, {month} {day.deleveryDay}, {year}
+                </p>} 
                 <span className={`price-carItem${pageClassName}`}>{Number.isInteger(item.price)? item.price+'.00':item.price}$</span>
                 <QuantityControler cart={cart} itemId={item.id} className={`quantity-cartItem${pageClassName}`} quantity={item.orderedQuantity}/>
                 {toRemove ?
