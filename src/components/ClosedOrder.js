886c4bd310dfa6f6e1df9f95cedad59eaf414374
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2023-11-03 15:36:23 +0100

    Nest a header element inside the container

--- src/components/ClosedOrder.js
+++ src/components/ClosedOrder.js
@@ -1,6 +1,9 @@
 export default function ClosedOrder(){
     return(
         <div className="container--closed-order">
+            <header className="header--closed-order">
+                Closed Order header
+            </header>
             
         </div>
     )
