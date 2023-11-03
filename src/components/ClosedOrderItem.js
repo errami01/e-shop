73dc985fdfc031156f57b7d42e03168c8742f9fe
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2023-11-03 17:13:03 +0100

    Nest a div inside container to display quantity table

--- src/components/ClosedOrderItem.js
+++ src/components/ClosedOrderItem.js
@@ -2,8 +2,9 @@ import "./ClosedOrderItem.css"
 export default function ClosedOrderItem({item}){
     return(
         <div className="container--closed-order-item">
-            <h5 className="item-title--closed-order-item"></h5>
+            <h5 className="item-title--closed-order-item">The order date</h5>
             <img className={`img-closed-order-item`} src={item.image} alt='cart item'/>
-        </div>
+            <div className="quantity-table--closed-order-item"></div>
+        </div >
     )
 }
