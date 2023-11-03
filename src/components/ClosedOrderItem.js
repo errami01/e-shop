55728ad923902b12e777e26836d1bba05cd4e9c9
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2023-11-03 17:17:07 +0100

    Nest a span inside quantity-table to display quantity value

--- src/components/ClosedOrderItem.js
+++ src/components/ClosedOrderItem.js
@@ -5,7 +5,8 @@ export default function ClosedOrderItem({item}){
             <h5 className="item-title--closed-order-item">The order date</h5>
             <img className={`img-closed-order-item`} src={item.image} alt='cart item'/>
             <div className="quantity-table--closed-order-item">
-                <span className="quantity-label--closed-order-item"></span>
+                <span className="quantity-label--closed-order-item">quantity</span>
+                <span className="quantity-value--closed-order-item">20</span>
             </div>
         </div >
     )
