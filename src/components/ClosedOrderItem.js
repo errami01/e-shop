0533c7c55becff01582d7d51cd0734556d3be532
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2023-11-03 15:53:34 +0100

    Nest an img element inside container

--- src/components/ClosedOrderItem.js
+++ src/components/ClosedOrderItem.js
@@ -2,7 +2,7 @@ import "./ClosedOrderItem.css"
 export default function ClosedOrderItem({item}){
     return(
         <div className="container--closed-order-item">
-
+            <img className={`img-closed-order-item`} src={item.image} alt='cart item'/>
         </div>
     )
 }
