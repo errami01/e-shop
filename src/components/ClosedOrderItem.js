86a770bbf22bd2fa41ffcc85ad1cc20525b2b8fc
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2023-11-03 15:55:17 +0100

    Nest an h5 element inside the container to display item title

--- src/components/ClosedOrderItem.js
+++ src/components/ClosedOrderItem.js
@@ -2,6 +2,7 @@ import "./ClosedOrderItem.css"
 export default function ClosedOrderItem({item}){
     return(
         <div className="container--closed-order-item">
+            <h5 className="item-title--closed-order-item"></h5>
             <img className={`img-closed-order-item`} src={item.image} alt='cart item'/>
         </div>
     )
