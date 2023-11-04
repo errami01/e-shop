ca7943b7b95e0f31f459127ede6b00a1a0c0d64a
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2023-11-04 08:47:28 +0100

    Replace placeholder data by item data

--- src/components/ClosedOrderItem.js
+++ src/components/ClosedOrderItem.js
@@ -2,15 +2,17 @@ import "./ClosedOrderItem.css"
 export default function ClosedOrderItem({item}){
     return(
         <div className="container--closed-order-item">
-            <h5 className="item-title--closed-order-item">The order date</h5>
-            <img className={`img-closed-order-item`} src={item.image} alt='cart item'/>
+            <h4 className="item-title--closed-order-item">{item.title}</h4>
+            <div className="img-container--closed-order-item">
+                <img className={`img--closed-order-item`} src={item.image} alt='cart item'/>
+            </div>
             <div className="quantity-table--closed-order-item">
-                <span className="quantity-label--closed-order-item">quantity</span>
-                <span className="quantity-value--closed-order-item">20</span>
+                <span className="quantity-label--closed-order-item">Quantity</span>
+                <span className="quantity-value--closed-order-item">{item.orderedQuantity}</span>
             </div>
             <div className="price-table--closed-order-item">
                 <span className="price-label--closed-order-item">Price</span>
-                <span className="price-value--closed-order-item">20</span>
+                <span className="price-value--closed-order-item">{Number.isInteger(item.price)? item.price+'.00':item.price}$</span>
             </div>
         </div >
     )
