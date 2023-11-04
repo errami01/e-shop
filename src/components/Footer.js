ee463dfbc7ebbdd3770aa7a55f39180e03aa8735
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2023-11-04 22:39:22 +0100

    Nest a span inside footer to display copyright

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import "./Footer.css"
 export default function Footer(){
     return(
         <footer className="container--footer">
-
+            <span className="copyright--footer">Copyright © 2023 - All righs reserved</span>
         </footer>
     )
 } 
