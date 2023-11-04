c898ca39b248abce35b38043f7cab16e89f2e4de
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2023-11-04 08:58:14 +0100

    Extract items from props and render a ClosedOrderItem component for each item

--- src/components/ClosedOrder.js
+++ src/components/ClosedOrder.js
@@ -1,10 +1,15 @@
-export default function ClosedOrder(){
+import ClosedOrderItem from "./ClosedOrderItem";
 import './ClosedOrder.css'
+export default function ClosedOrder({items}){
+    const closedOrderItems = items.map((item, index)=>{
+        return <ClosedOrderItem key= {index} item = {item}/>
+    })
     return(
         <div className="container--closed-order">
             <h4 className="header--closed-order">
                 Closed Order header
             </h4>
+            {closedOrderItems}
         </div>
     )
 } 
