70751790c8587a4a5f53804ddb8825b4778df129
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2023-11-04 09:49:19 +0100

    Render closedOrders or EmptyCustPage depending of loaderData value

--- src/pages/customer/ClosedOrders.js
+++ src/pages/customer/ClosedOrders.js
@@ -13,12 +13,17 @@ export default function ClosedOrders(){
     })
     return(
         <div className='container--closedOrders'>
-            <EmptyCustPage
-                icon={<i className="fa-solid fa-cart-flatbed-suitcase"></i>}
-                notification='No history to display'
-                explanation='All your closed orders will be saved here.'
-                btnText='START SHOPPING'
-            />
+            {
+                loaderData?
+                    closedOrders
+                :
+                    <EmptyCustPage
+                    icon={<i className="fa-solid fa-cart-flatbed-suitcase"></i>}
+                    notification='No history to display'
+                    explanation='All your closed orders will be saved here.'
+                    btnText='START SHOPPING'
+                />
+            }
         </div>
     )
 }
