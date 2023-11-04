11c616ad95bb920088e0e8c22b597dda6d058865
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2023-11-04 09:45:02 +0100

    Map over loaderData values and return a ClosedOrder component for each value

--- src/pages/customer/ClosedOrders.js
+++ src/pages/customer/ClosedOrders.js
@@ -1,12 +1,16 @@
 import { useLoaderData } from 'react-router-dom'
 import EmptyCustPage from '../../components/EmptyCustPage'
 import { getClosedOrders } from '../../utils/fetcher'
+import ClosedOrder from '../../components/ClosedOrder'
 
 export async function loader(){
     return await getClosedOrders()
 } 
 export default function ClosedOrders(){
     const loaderData = useLoaderData()
+    const closedOrders = Object.values(loaderData).map((arr)=>{
+        return <ClosedOrder items={arr}/>
+    })
     return(
         <div className='container--closedOrders'>
             <EmptyCustPage
