3baf3d8f263a45add2b3cbf20fad2c9a26235cd4
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2023-11-04 12:45:42 +0100

    Replace placeholders text by data from orders object

--- src/components/ClosedOrder.js
+++ src/components/ClosedOrder.js
@@ -1,20 +1,20 @@
 import ClosedOrderItem from "./ClosedOrderItem";
 import './ClosedOrder.css'
-export default function ClosedOrder({items}){
-    const closedOrderItems = items.map((item, index)=>{
-        return <ClosedOrderItem key= {index} item = {item}/>
+export default function ClosedOrder({orders}){
+    const closedOrderItems = orders.cart.map((order, index)=>{
+        return <ClosedOrderItem key= {index} item = {order}/>
     })
     return(
         <div className="container--closed-order">
             <h4 className="header--closed-order">
-                Closed Order header
+                {orders.time}
             </h4>
             {closedOrderItems}
             <div className="totals--closed-order">
                 <span className="total-items-label--closed-order">Total Items</span>
-                <span className="total-items--closed-order">100</span>
+                <span className="total-items--closed-order">{orders.totalItems}</span>
                 <span className="total-amount-label--closed-order">Total</span>
-                <span className="total-amount--closed-order">100</span>
+                <span className="total-amount--closed-order">{Number.isInteger(orders.totalAmount)? orders.totalAmount+'.00':orders.totalAmount}$</span>
             </div>
         </div>
     )
